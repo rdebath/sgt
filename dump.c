ae54caaf1ee586e859883da55d98a6eea6458eab
Simon Tatham <anakin@pobox.com>
2000-01-20 15:46:52 +0000

    Tidy up code for modern day (bit of formatting; go over to using fread and memset rather than doing things by hand; proper ANSI function definitions). Also fixed at least one bug in the process (use of -o caused no ASCII printout on first line of dump) and possibly another one (bizarre use of ungetc might have caused some subtle eof problems on special files; behaviour is now simple and clean with fread).
    git-svn-id: svn://svn.tartarus.org/sgt@373 cda61777-01e9-0310-a592-d414129be87e

--- dump.c
+++ dump.c
@@ -1,8 +1,88 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int lower=0, squash=1, ascii=1;
-long offset=0, start=0, linewidth=16;
+struct dumpstate {
+    int lower;                         /* display in lower case? */
+    int squash;                        /* coalesce repeated lines? */
+    int ascii;                         /* display ASCII on RHS of hex? */
+    long offset;                       /* offset at which to start new line */
+    long start;                        /* add this to all displayed offsets */
+    long linewidth;                    /* bytes to dump per display line */
+};
+
+void dump (struct dumpstate state, FILE *fp) {   
+    long addr=0;
+    int i, j;
+    unsigned char *buffer;
+    char *prl, *oldprl;
+    char *fmt, *hex;
+    int etcflag = 0;
+    int byteoffset;
+
+    if (state.lower) {
+        hex = "0123456789abcdef";
+        fmt = "%08lx  %s\n";
+    } else {
+        hex = "0123456789ABCDEF";
+        fmt = "%08lX  %s\n";
+    }
+
+    buffer = malloc (state.linewidth);
+    prl = malloc (state.linewidth*4+17); /* Should be enough :) */
+    oldprl = malloc (state.linewidth*4+17);
+
+    if (!buffer || !prl || !oldprl) {
+	fprintf (stderr, "Out of memory.\n");
+	exit (1);
+    }
+
+    if (state.offset)
+	byteoffset = state.linewidth - state.offset;
+    else
+	byteoffset = 0;
+
+    while (1) {
+        i = fread(buffer + byteoffset, 1, state.linewidth - byteoffset, fp);
+        if (i <= 0)                    /* finished reading data */
+            break;
+
+	if (i) {
+            i += byteoffset;
+
+	    memset(prl, ' ', state.linewidth*4+17);
+
+	    for (j = byteoffset; j < i; j++) {
+		prl[j*3]=hex[buffer[j]/16];
+		prl[j*3+1]=hex[buffer[j]%16];
+	    }
+
+	    if (state.ascii) {
+		for (j = byteoffset; j < i; j++) {
+		    if (buffer[j] >= 32 && buffer[j] <= 126)
+			prl[j+state.linewidth*3+1]=buffer[j];
+		    else
+			prl[j+state.linewidth*3+1]='.';
+		}
+		prl[i+state.linewidth*3+1]='\0';
+	    } else
+		prl[3*i-1] = '\0';
+
+	    if (state.squash && !strcmp(oldprl, prl) && !feof(fp)) {
+		if (!etcflag) {
+		    etcflag = 1;
+		    printf("etc...\n");
+		}
+	    } else {
+		etcflag = 0;
+		printf (fmt, addr + state.start, prl);
+		strcpy (oldprl, prl);
+	    }
+	    addr += state.linewidth - byteoffset;
+	}
+	byteoffset = 0;
+    }
+    free (buffer);
+}
 
 void help(void) {
     static char *helptext[] = {
@@ -24,12 +104,12 @@ void help(void) {
 	fputs (*p++, stderr);
 }
 
-main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
     FILE *fp;
     int i, j;
     int flag;
     char **names;
+    struct dumpstate state;
 
     flag = 0;
     names = malloc(argc*sizeof(*names));
@@ -37,6 +117,13 @@ main(int argc, char **argv)
 	perror("malloc");
 	return 1;
     }
+
+    state.lower = 0;
+    state.squash = 1;
+    state.ascii = 1;
+    state.offset = 0;
+    state.start = 0;
+    state.linewidth = 16;
 
     j = 0;
     while (--argc > 0) {
@@ -49,13 +136,13 @@ main(int argc, char **argv)
 	    p++;
 	    while ( (c = *p++) ) switch (c) {
 	      case 'l':		       /* lower case */
-		lower = 1;
+		state.lower = 1;
 		break;
 	      case 'f':		       /* force full display */
-		squash = 0;
+		state.squash = 0;
 		break;
 	      case 'a':		       /* disable ascii display */
-		ascii = 0;
+		state.ascii = 0;
 		break;
 	      case 'h':		       /* help text */
 	      case '?':
@@ -75,13 +162,13 @@ main(int argc, char **argv)
 		}
 		switch (c) {
 		  case 'o':
-		    offset = strtoul(v,NULL,0);
+		    state.offset = strtoul(v,NULL,0);
 		    break;
 		  case 's':
-		    start = strtoul(v,NULL,0);
+		    state.start = strtoul(v,NULL,0);
 		    break;
 		  case 'w':
-		    linewidth = strtoul(v,NULL,0);
+		    state.linewidth = strtoul(v,NULL,0);
 		    break;
 		}
 		break;
@@ -94,107 +181,20 @@ main(int argc, char **argv)
 	    names[j++] = p;
     }
 
-    offset=offset%linewidth;
+    state.offset %= state.linewidth;
 
-    for (i = 0; i < j; i++)
-    {
+    for (i = 0; i < j; i++) {
 	flag=1;
 	fp = fopen (names[i], "r");
 	if (fp == NULL)
-	    printf ("dump: Couldn't open file %s\n", names[i]);
-	else
-	{
-	    dump (fp);
+	    printf ("dump: %s: unable to open\n", names[i]);
+	else {
+	    dump (state, fp);
 	    fclose (fp);
 	}
     }
     if (!flag)
-	dump (stdin);
+	dump (state, stdin);
+
+    return 0;
 }	    
-
-dump (FILE *fp)
-{   
-    long addr=0;
-    int i, j, c, flag=1;
-
-    unsigned char hex[16]="0123456789abcdef";
-    unsigned char *buffer;
-    unsigned char *pbuf;
-    unsigned char *prl, *oldprl;
-    char *fmt = lower ? "%08lx  %s\n" : "%08lX  %s\n" ;
-    int g, etcflag = 0;
-    int byteoffset;
-    
-    buffer = malloc (linewidth);
-    pbuf = malloc (linewidth+1);
-    prl = malloc (linewidth*4+17); /* Should be enough :) */
-    oldprl = malloc (linewidth*4+17);
-    if (!buffer || !pbuf || !prl || !oldprl)
-    {
-	fprintf (stderr, "Out of memory.\n");
-	exit (1);
-    }
-    if (!lower)
-	for (i = 10;i<16;i++)
-	    hex[i]=hex[i]&95;
-    if (offset)
-	byteoffset=linewidth-offset;
-    else
-	byteoffset=0;
-    while (flag)
-    {
-	i=byteoffset;
-	while (flag && i<linewidth)
-	{
-	    g = fgetc(fp);
-	    if (g == EOF)
-		flag=0;
-	    else
-	    {
-		buffer[i]=g;
-		i++;
-	    }
-	}
-	if (i)
-	{
-	    for (c = 0; c < linewidth*3+3; c++)
-		prl[c]=' ';
-	    for (j = byteoffset; j < i; j++)
-	    {
-		prl[j*3]=hex[buffer[j]/16];
-		prl[j*3+1]=hex[buffer[j]%16];
-	    }
-	    if (ascii) {
-		prl[i+linewidth*3+1]='\0';
-		for (j=0; j < byteoffset; j++)
-		    pbuf[j]=' ';
-		for (j = byteoffset; j < i; j++)
-		{
-		    if (31<buffer[j] && 127>buffer[j])
-			prl[j+linewidth*3+1]=buffer[j];
-		    else
-			prl[j+linewidth*3+1]='.';
-		}
-	    } else
-		prl[3*i-1] = '\0';
-
-	    if ( (c = fgetc(fp)) != EOF )
-		ungetc (c, fp) ;
-
-	    if (squash && !strcmp(oldprl, prl) && !feof(fp)) {
-		if (!etcflag) {
-		    etcflag = 1 ;
-		    printf("etc...\n") ;
-		}
-	    } else {
-		etcflag = 0 ;
-		printf (fmt, addr+start, prl);
-		strcpy (oldprl, prl) ;
-	    }
-	    addr+=linewidth-byteoffset;
-	}
-	byteoffset=0;
-    }
-    free (buffer);
-    free (pbuf);
-}
