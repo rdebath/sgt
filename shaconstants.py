eb7a57af96fa99916e15f846e69f5f4238634a35
Simon Tatham <anakin@pobox.com>
2000-10-20 16:07:13 +0000

    Finish implementation
    git-svn-id: svn://svn.tartarus.org/sgt@736 cda61777-01e9-0310-a592-d414129be87e

--- shaconstants.py
+++ shaconstants.py
@@ -42,5 +42,43 @@ def cuberoot(N):
 
     return x
 
-print hex(squareroot(2L << (2*64)))
-print hex(cuberoot(2L << (3*64)))
+def sieve(n):
+    z = []
+    list = []
+    for i in range(n): z.append(1)
+    for i in range(2,n):
+        if z[i]:
+            list.append(i)
+            for j in range(i,n,i): z[j] = 0
+    return list
+
+def hexstr(n):
+    s = hex(n)
+    if s[-1] == "L":
+	s = s[:-1]
+    if s[0:2] != "0x":
+	s = "0x" + s
+    return s
+
+primes = sieve(512)
+assert len(primes) >= 80
+
+print "SHA-256 initialisation vector:"
+for p in primes[0:8]:
+    print hexstr(0xFFFFFFFFL & squareroot(p * (1L << (2*32))))
+
+print "SHA-256 per-round constants:"
+for p in primes[0:64]:
+    print hexstr(0xFFFFFFFFL & cuberoot(p * (1L << (3*32))))
+
+print "SHA-512 initialisation vector:"
+for p in primes[0:8]:
+    print hexstr(0xFFFFFFFFFFFFFFFFL & squareroot(p * (1L << (2*64))))
+
+print "SHA-512 per-round constants:"
+for p in primes[0:80]:
+    print hexstr(0xFFFFFFFFFFFFFFFFL & cuberoot(p * (1L << (3*64))))
+
+print "SHA-384 initialisation vector:"
+for p in primes[8:16]:
+    print hexstr(0xFFFFFFFFFFFFFFFFL & squareroot(p * (1L << (2*64))))
