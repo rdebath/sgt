8c16e6f72307d542c248f4686c13e564c49ce634
Simon Tatham <anakin@pobox.com>
2001-09-24 08:52:42 +0000

    Use the >= rather than > comparison, to stop the output differing _needlessly_ from that of the greedy algorithm; and also add a verbose switch for algorithm tracing.
    git-svn-id: svn://svn.tartarus.org/sgt@1292 cda61777-01e9-0310-a592-d414129be87e

--- misc/wrap.py
+++ misc/wrap.py
@@ -5,13 +5,25 @@
 import sys
 import string
 
+width = 72
+verbose = 0
+
 def wrap(list):
     class holder:
 	"container class"
 	pass
+    if verbose:
+	print "width =", width
+	print "word lengths:"
+	lens = []
+	for i in list:
+	    lens.append(len(i))
+	print lens
     n = len(list)
     z = []
     for i in range(n-1, -1, -1):
+	if verbose:
+	    print "wrapping from word", i
 	wid = -1
 	best = None
 	bestcost = None
@@ -22,9 +34,13 @@ def wrap(list):
 		break
 	    if j >= len(z):
 		cost = 0
+		if verbose:
+		    print j, "words: end of line, cost", cost
 	    else:
 		cost = (width-wid) ** 2 + z[j].cost
-	    if bestcost == None or bestcost > cost:
+		if verbose:
+		    print j, "words: cost", (width-wid) ** 2, "+", z[j].cost, "=", cost
+	    if bestcost == None or bestcost >= cost:
 		bestcost = cost
 		best = j
 	if best == None:
@@ -33,6 +49,8 @@ def wrap(list):
 	else:
 	    z[0].cost = bestcost
 	    z[0].n = best
+	if verbose:
+	    print "optimal:", z[0].n, "words, at cost", z[0].cost
     i = 0
     while i < len(list):
 	n = z[i].n
@@ -56,13 +74,13 @@ def dofile(f):
     if len(para) > 0:
 	wrap(para)
 
-width = 72
-
 donefile = 0
 for arg in sys.argv[1:]:
     if arg[0] == "-":
 	if arg[:2] == "-w":
 	    width = string.atoi(arg[2:])
+	elif arg[:2] == "-v":
+	    verbose = 1
 	else:
 	    print "wrap: unknown option " + arg[:2]
 	    print "usage: wrap [-wWIDTH] [file [file...]]"
