6b7db8209ecfb46d50ec6264da87c821aedc5302
Simon Tatham <anakin@pobox.com>
2002-08-29 18:30:43 +0000

    Various changes: add swash_width() utility function, make swash_text() return void as it should, and fix the Y which was the wrong shape.
    git-svn-id: svn://svn.tartarus.org/sgt@1889 cda61777-01e9-0310-a592-d414129be87e

--- swash.h
+++ swash.h
@@ -1,2 +1,3 @@
-int swash_text(int x, int y, char *text,
-	       void (*plot)(void *ctx, int x, int y), void *plotctx);
+void swash_text(int x, int y, char *text,
+		void (*plot)(void *ctx, int x, int y), void *plotctx);
+int swash_width(char *text);
