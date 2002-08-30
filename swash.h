384ff0639ed239a759bc0f3488413a1efc1c12bc
Simon Tatham <anakin@pobox.com>
2002-08-30 18:16:21 +0000

    Useful function swash_centre() moved into swash.c for easy reuse
    git-svn-id: svn://svn.tartarus.org/sgt@1893 cda61777-01e9-0310-a592-d414129be87e

--- swash.h
+++ swash.h
@@ -1,3 +1,5 @@
 void swash_text(int x, int y, char *text,
 		void (*plot)(void *ctx, int x, int y), void *plotctx);
+void swash_centre(int xmin, int xmax, int y, char *text,
+		  void (*plot)(void *ctx, int x, int y), void *plotctx);
 int swash_width(char *text);
