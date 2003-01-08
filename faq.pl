2d62700a27c1c010443d159e9b9dd2f5a3fcfc67
Simon Tatham <anakin@pobox.com>
2003-01-08 13:23:47 +0000

    Initial checkin to bring the PuTTY web pages under CVS.
    git-svn-id: svn://svn.tartarus.org/sgt@2496 cda61777-01e9-0310-a592-d414129be87e

---
+++ faq.pl
@@ -0,0 +1,55 @@
+#!/usr/bin/perl
+
+print << 'EOF';
+<HTML><HEAD>
+<TITLE>PuTTY FAQ</TITLE>
+</HEAD>
+<link rel="stylesheet" type="text/css" href="sitestyle.css" name="PuTTY Home Page Style">
+<BODY>
+<h1 align=center>PuTTY FAQ</h1>
+
+<p align=center>
+<a href="./">Home</a> |
+<a href="licence.html">Licence</a> |
+<b>FAQ</b> |
+<a href="docs.html">Docs</a> |
+<a href="download.html">Download</a> |
+<a href="keys.html">Keys</a><br>
+<a href="mirrors.html">Mirrors</a> |
+<a href="maillist.html">Updates</a> |
+<a href="feedback.html">Feedback</a> |
+<a href="changes.html">Changes</a> |
+<a href="wishlist.html">Wishlist</a></p>
+EOF
+
+open FAQ, $ENV{"HOME"} . "/pub/putty/doc/AppendixA.html";
+while (<FAQ>) {
+  last if /<body>/i;
+}
+# The line after <body> is the navigation links within the Halibut output
+<FAQ>;
+
+while (<FAQ>) {
+  # The bottom navigation links terminate the FAQ material;
+  # conveniently, they use single quotes in the hrefs instead of
+  # double.
+  last if /^<p><a href='/i;
+  # for each line, we must rewrite hrefs
+  s/<a href="([^"\/]*)">/'<a href="' . &fix_href($1) . '">'/eg;
+  print;
+}
+
+print << 'EOF';
+
+<p><hr>If you want to comment on this web site, see the
+<a href="feedback.html">Feedback page</a>.
+<br>
+(last modified on <!--LASTMOD-->[insert date here]<!--END-->)
+</BODY></HTML>
+EOF
+
+sub fix_href {
+  local ($_) = @_;
+  s/^AppendixA.html// or s/^/http:\/\/www.tartarus.org\/~simon\/puttydoc\//;
+  $_;
+}
