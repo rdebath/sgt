09cf29bff40e228a72e5e9863634dcab7f399fb6
Simon Tatham <anakin@pobox.com>
2003-06-01 11:21:36 +0000

    How CVS can possibly think it's sensible to restrict `cvs admin' commands to the members of group cvsadmin even for a user's private repository, _or_ that it's feasible to try to enforce this in a non-setuid program, I have no idea. Fortunately, the latter mitigates the former, so here's an LD_PRELOAD-based fix. :-)
    git-svn-id: svn://svn.tartarus.org/sgt@3235 cda61777-01e9-0310-a592-d414129be87e

---
+++ misc/cvsadmin.c
@@ -0,0 +1,28 @@
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+#include <grp.h>
+#include <dlfcn.h>
+
+/*
+ * Small shared library for Linux, to be loaded with LD_PRELOAD.
+ * Overrides getgrnam() to pretend the `cvsadmin' group does not
+ * exist. Good for persuading CVS that you _are_ allowed to do CVS
+ * admin functions on your own private repositories!
+ *
+ * compilation (linux):
+ *   gcc -fpic -c cvsadmin.c && ld -shared -o cvsadmin.so cvsadmin.o
+ * use:
+ *   LD_PRELOAD=/full/path/name/to/cvsadmin.so cvs admin <stuff>
+ */
+
+struct group *getgrnam(const char *name)
+{
+    if (!strcmp(name, "cvsadmin")) {
+	return NULL;
+    } else {
+	char buf[FILENAME_MAX < 512 ? 512 : FILENAME_MAX];
+	struct group *(*real_getgrnam) (const char *name);
+	real_getgrnam = dlsym(RTLD_NEXT, "getgrnam");
+    }
+}
