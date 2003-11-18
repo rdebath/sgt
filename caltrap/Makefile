da9cffdab83040421f630f5a7dd6871db82626c2
Simon Tatham <anakin@pobox.com>
2003-11-18 10:47:16 +0000

    Caltrap, a piece of calendar software built on SQLite. Initial application framework generated by `skel'.
    git-svn-id: svn://svn.tartarus.org/sgt@3545 cda61777-01e9-0310-a592-d414129be87e

---
+++ caltrap/Makefile
@@ -0,0 +1,58 @@
+# Caltrap master makefile
+
+# Requires a compiler with -MD support, currently
+
+# `make' from top level will build in caltrap.b
+# `make BUILDDIR=foo' from top level will build in directory foo
+ifndef REALBUILD
+ifndef BUILDDIR
+BUILDDIR := build
+endif
+all:
+	@test -d $(BUILDDIR) || mkdir $(BUILDDIR)
+	@make -C $(BUILDDIR) -f ../Makefile REALBUILD=yes
+else
+
+# The `real' makefile part.
+
+ifdef RELEASE
+ifndef VERSION
+VERSION := $(RELEASE)
+endif
+else
+CFLAGS += -g
+endif
+
+
+
+
+ifndef VER
+ifdef VERSION
+VER := $(VERSION)
+endif
+endif
+ifdef VER
+VDEF := -DVERSION=\"$(VER)\"
+endif
+
+SRC := ../
+
+MODULES := main malloc error help licence version
+
+OBJECTS := $(addsuffix .o,$(MODULES))
+DEPS := $(addsuffix .d,$(MODULES))
+
+caltrap: $(OBJECTS)
+	$(CC) $(LFLAGS) -o caltrap $(OBJECTS) $(LIBS)
+
+%.o: $(SRC)%.c
+	$(CC) $(CFLAGS) -MD -c $<
+
+version.o: FORCE
+	$(CC) $(VDEF) -MD -c $(SRC)version.c
+
+FORCE: # phony target to force version.o to be rebuilt every time
+
+-include $(DEPS)
+
+endif
---
+++ caltrap/error.c
@@ -0,0 +1,56 @@
+/*
+ * error.c: caltrap error handling
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include "caltrap.h"
+
+/*
+ * Error flags
+ */
+#define PREFIX 0x0001		       /* give `caltrap:' prefix */
+
+static void do_error(int code, va_list ap) {
+    char error[1024];
+    char *sp;
+    int flags;
+
+    switch(code) {
+      case err_nomemory:	       /* no arguments */
+	sprintf(error, "out of memory");
+	flags = PREFIX;
+	break;
+      case err_optnoarg:
+	sp = va_arg(ap, char *);
+	sprintf(error, "option `-%.200s' requires an argument", sp);
+	flags = PREFIX;
+	break;
+      case err_nosuchopt:
+	sp = va_arg(ap, char *);
+	sprintf(error, "unrecognised option `-%.200s'", sp);
+	flags = PREFIX;
+	break;
+    }
+
+    if (flags & PREFIX)
+	fputs("caltrap: ", stderr);
+    fputs(error, stderr);
+    fputc('\n', stderr);
+}
+
+void fatal(int code, ...) {
+    va_list ap;
+    va_start(ap, code);
+    do_error(code, ap);
+    va_end(ap);
+    exit(EXIT_FAILURE);
+}
+
+void error(int code, ...) {
+    va_list ap;
+    va_start(ap, code);
+    do_error(code, ap);
+    va_end(ap);
+}
