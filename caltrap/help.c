6640b6b1fc558d63538b42aaffef83b3142fef33
Simon Tatham <anakin@pobox.com>
2003-11-20 11:36:37 +0000

    Fix display bug in the help.
    git-svn-id: svn://svn.tartarus.org/sgt@3558 cda61777-01e9-0310-a592-d414129be87e

--- caltrap/help.c
+++ caltrap/help.c
@@ -14,7 +14,7 @@ static char *usagetext[] = {
     "usage: caltrap -a <date> [<time>]    add entry (enter text on stdin)",
     "       caltrap -l <date> [<date>]    list entries (for a date or range)",
     "       caltrap -C <time> <cmd>       list entries in the next <time>",
-    "                                     and pipe them into <cmd>"
+    "                                         and pipe them into <cmd>",
     "       caltrap --init                set up an empty database",
     "options:",
     "       -D <dbpath>                   use a db other than ~/.caltrapdb",
