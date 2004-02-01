7eb0fd21564ee090a34878c33a78b67b8bfb9af0
Simon Tatham <anakin@pobox.com>
2004-02-01 09:47:04 +0000

    Some OO infrastructure, the `all' command to generate all the polyhedron files we can, and a generic edge-dual finding function which automatically generates the two rhombic solids.
    git-svn-id: svn://svn.tartarus.org/sgt@3780 cda61777-01e9-0310-a592-d414129be87e

--- regular/mkregular.py
+++ regular/mkregular.py
@@ -5,8 +5,71 @@
 import sys
 import math
 
-nvertices = 0
-vertices = {}
+class polyhedron:
+    def __init__(self):
+	self.vertices = {}
+	self.vlist = []
+	self.faces = {}
+	self.normals = {}
+	self.flist = []
+
+    def vertex(self, x, y, z):
+	vnum = len(self.vertices)
+	vname = lettername(vnum)
+	x, y, z = float(x), float(y), float(z)
+	self.vertices[vname] = x, y, z
+	self.vlist.append(vname)
+	return vname
+
+    def facelist(self, vlist):
+	# Construct a face, given an ordered list of vertices. We
+	# assume for these polyhedra that all faces point away from the
+	# origin, which means we can auto-compute the surface normal
+	# and automatically arrange for the face list to be the right
+        # way round.
+        vlist = list(tuple(vlist))
+        vprod = (0,0,0)
+        centroid = (0,0,0)
+        for i in range(len(vlist)):
+            # Compute an approximation to the surface normal given
+            # the three vertices starting at this point.
+            va = self.vertices[vlist[i]]
+            vb = self.vertices[vlist[(i+1) % len(vlist)]]
+            vc = self.vertices[vlist[(i+2) % len(vlist)]]
+            dx1, dy1, dz1 = vb[0]-va[0], vb[1]-va[1], vb[2]-va[2]
+            dx2, dy2, dz2 = vc[0]-vb[0], vc[1]-vb[1], vc[2]-vb[2]
+            vp = (dy1*dz2-dz1*dy2, dz1*dx2-dx1*dz2, dx1*dy2-dy1*dx2)
+            vprod = (vprod[0]+vp[0], vprod[1]+vp[1], vprod[2]+vp[2])
+            centroid = (centroid[0]+va[0], centroid[1]+va[1], centroid[2]+va[2])
+        # If the normal vector points inwards, invert it and reverse the list.
+        if vprod[0]*centroid[0] + vprod[1]*centroid[1] + vprod[2]*centroid[2] < 0:
+            vprod = (-vprod[0], -vprod[1], -vprod[2])
+            vlist.reverse()
+        # Normalise the normal vector to unit length.
+        vplen = math.sqrt(vprod[0]**2 + vprod[1]**2 + vprod[2]**2)
+        vprod = (vprod[0]/vplen, vprod[1]/vplen, vprod[2]/vplen)
+        # Compute the face name.
+        facename = ""
+        for s in vlist:
+            facename = facename + s
+        # Output.
+	self.faces[facename] = vlist
+	self.normals[facename] = vprod
+	self.flist.append(facename)
+
+    def face(self, *list):
+	self.facelist(list)
+
+    def output(self, file):
+	for v in self.vlist:
+	    x, y, z = self.vertices[v]
+	    file.write("point "+v+" "+repr(x)+" "+repr(y)+" "+repr(z)+"\n")
+	for f in self.flist:
+	    vlist = self.faces[f]
+	    for s in vlist:
+		file.write("face " + f + " " + s + "\n")
+	    x, y, z = self.normals[f]
+	    file.write("normal "+f+" "+repr(x)+" "+repr(y)+" "+repr(z)+"\n")
 
 def lettername(n):
     chars = 1
@@ -23,109 +86,69 @@ def lettername(n):
             s = chr(97 + k) + s
     return s
 
-def vertex(x, y, z):
-    global nvertices
-    vnum = len(vertices)
-    vname = lettername(vnum)
-    x, y, z = float(x), float(y), float(z)
-    print "point", vname, x, y, z
-    vertices[vname] = x, y, z
-    return vname
-
-def facelist(vlist):
-    # Construct a face, given an ordered list of vertices. We
-    # assume for these polyhedra that all faces point away from the
-    # origin, which means we can auto-compute the surface normal
-    # and automatically arrange for the face list to be the right
-    # way round.
-    vlist = list(tuple(vlist))
-    vprod = (0,0,0)
-    centroid = (0,0,0)
-    for i in range(len(vlist)):
-        # Compute an approximation to the surface normal given
-        # the three vertices starting at this point.
-        va = vertices[vlist[i]]
-        vb = vertices[vlist[(i+1) % len(vlist)]]
-        vc = vertices[vlist[(i+2) % len(vlist)]]
-        dx1, dy1, dz1 = vb[0]-va[0], vb[1]-va[1], vb[2]-va[2]
-        dx2, dy2, dz2 = vc[0]-vb[0], vc[1]-vb[1], vc[2]-vb[2]
-        vp = (dy1*dz2-dz1*dy2, dz1*dx2-dx1*dz2, dx1*dy2-dy1*dx2)
-        vprod = (vprod[0]+vp[0], vprod[1]+vp[1], vprod[2]+vp[2])
-        centroid = (centroid[0]+va[0], centroid[1]+va[1], centroid[2]+va[2])
-    # If the normal vector points inwards, invert it and reverse the list.
-    if vprod[0]*centroid[0] + vprod[1]*centroid[1] + vprod[2]*centroid[2] < 0:
-        vprod = (-vprod[0], -vprod[1], -vprod[2])
-        vlist.reverse()
-    # Normalise the normal vector to unit length.
-    vplen = math.sqrt(vprod[0]**2 + vprod[1]**2 + vprod[2]**2)
-    vprod = (vprod[0]/vplen, vprod[1]/vplen, vprod[2]/vplen)
-    # Compute the face name.
-    facename = ""
-    for s in vlist:
-        facename = facename + s
-    # Output.
-    for s in vlist:
-        print "face", facename, s
-    print "normal", facename, vprod[0], vprod[1], vprod[2]
-
-def face(*list):
-    facelist(list)
 
 def tetrahedron():
+    p = polyhedron()
     # Easiest way to define a tetrahedron is to take four vertices
     # from the cube.
-    a = vertex(-1, -1, -1)
-    b = vertex(-1, +1, +1)
-    c = vertex(+1, +1, -1)
-    d = vertex(+1, -1, +1)
-    face(a, b, c)
-    face(b, c, d)
-    face(c, d, a)
-    face(d, a, b)
+    a = p.vertex(-1, -1, -1)
+    b = p.vertex(-1, +1, +1)
+    c = p.vertex(+1, +1, -1)
+    d = p.vertex(+1, -1, +1)
+    p.face(a, b, c)
+    p.face(b, c, d)
+    p.face(c, d, a)
+    p.face(d, a, b)
+    return p
 
 def cube():
-    a = vertex(-1, -1, -1)
-    b = vertex(-1, -1, +1)
-    c = vertex(-1, +1, -1)
-    d = vertex(-1, +1, +1)
-    e = vertex(+1, -1, -1)
-    f = vertex(+1, -1, +1)
-    g = vertex(+1, +1, -1)
-    h = vertex(+1, +1, +1)
-    face(a, b, d, c)
-    face(e, f, h, g)
-    face(a, c, g, e)
-    face(b, d, h, f)
-    face(a, e, f, b)
-    face(c, g, h, d)
+    p = polyhedron()
+    a = p.vertex(-1, -1, -1)
+    b = p.vertex(-1, -1, +1)
+    c = p.vertex(-1, +1, -1)
+    d = p.vertex(-1, +1, +1)
+    e = p.vertex(+1, -1, -1)
+    f = p.vertex(+1, -1, +1)
+    g = p.vertex(+1, +1, -1)
+    h = p.vertex(+1, +1, +1)
+    p.face(a, b, d, c)
+    p.face(e, f, h, g)
+    p.face(a, c, g, e)
+    p.face(b, d, h, f)
+    p.face(a, e, f, b)
+    p.face(c, g, h, d)
+    return p
 
 def octahedron():
-    a = vertex(-1, 0, 0)
-    b = vertex(+1, 0, 0)
-    c = vertex(0, -1, 0)
-    d = vertex(0, +1, 0)
-    e = vertex(0, 0, -1)
-    f = vertex(0, 0, +1)
-    face(a, c, e)
-    face(a, c, f)
-    face(a, d, e)
-    face(a, d, f)
-    face(b, c, e)
-    face(b, c, f)
-    face(b, d, e)
-    face(b, d, f)
+    p = polyhedron()
+    a = p.vertex(-1, 0, 0)
+    b = p.vertex(+1, 0, 0)
+    c = p.vertex(0, -1, 0)
+    d = p.vertex(0, +1, 0)
+    e = p.vertex(0, 0, -1)
+    f = p.vertex(0, 0, +1)
+    p.face(a, c, e)
+    p.face(a, c, f)
+    p.face(a, d, e)
+    p.face(a, d, f)
+    p.face(b, c, e)
+    p.face(b, c, f)
+    p.face(b, d, e)
+    p.face(b, d, f)
+    return p
 
 def dodecahedron():
+    p = polyhedron()
     # Simplest way to define a dodecahedron is to raise a roof on
     # each face of a cube. So we start with the cube vertices.
-    a = vertex(-1, -1, -1)
-    b = vertex(-1, -1, +1)
-    c = vertex(-1, +1, -1)
-    d = vertex(-1, +1, +1)
-    e = vertex(+1, -1, -1)
-    f = vertex(+1, -1, +1)
-    g = vertex(+1, +1, -1)
-    h = vertex(+1, +1, +1)
+    a = p.vertex(-1, -1, -1)
+    b = p.vertex(-1, -1, +1)
+    c = p.vertex(-1, +1, -1)
+    d = p.vertex(-1, +1, +1)
+    e = p.vertex(+1, -1, -1)
+    f = p.vertex(+1, -1, +1)
+    g = p.vertex(+1, +1, -1)
+    h = p.vertex(+1, +1, +1)
     # This cube has side length 2. So the side length of the
     # dodecahedron will be 2/phi.
     phi = (1.0 + math.sqrt(5))/2.0
@@ -148,85 +171,197 @@ def dodecahedron():
     z = 1 + math.sqrt(side**2 - (1-side/2)**2 - 1)
     y = side/2
 
-    ab = vertex(-z, -y, 0)
-    cd = vertex(-z, +y, 0)
-    ef = vertex(+z, -y, 0)
-    gh = vertex(+z, +y, 0)
-    ae = vertex(0, -z, -y)
-    bf = vertex(0, -z, +y)
-    cg = vertex(0, +z, -y)
-    dh = vertex(0, +z, +y)
-    ac = vertex(-y, 0, -z)
-    eg = vertex(+y, 0, -z)
-    bd = vertex(-y, 0, +z)
-    fh = vertex(+y, 0, +z)
+    ab = p.vertex(-z, -y, 0)
+    cd = p.vertex(-z, +y, 0)
+    ef = p.vertex(+z, -y, 0)
+    gh = p.vertex(+z, +y, 0)
+    ae = p.vertex(0, -z, -y)
+    bf = p.vertex(0, -z, +y)
+    cg = p.vertex(0, +z, -y)
+    dh = p.vertex(0, +z, +y)
+    ac = p.vertex(-y, 0, -z)
+    eg = p.vertex(+y, 0, -z)
+    bd = p.vertex(-y, 0, +z)
+    fh = p.vertex(+y, 0, +z)
 
-    face(a, ab, b, bf, ae)
-    face(e, ef, f, bf, ae)
-    face(c, cd, d, dh, cg)
-    face(g, gh, h, dh, cg)
-    face(a, ac, c, cd, ab)
-    face(b, bd, d, cd, ab)
-    face(e, eg, g, gh, ef)
-    face(f, fh, h, gh, ef)
-    face(a, ae, e, eg, ac)
-    face(c, cg, g, eg, ac)
-    face(b, bf, f, fh, bd)
-    face(d, dh, h, fh, bd)
+    p.face(a, ab, b, bf, ae)
+    p.face(e, ef, f, bf, ae)
+    p.face(c, cd, d, dh, cg)
+    p.face(g, gh, h, dh, cg)
+    p.face(a, ac, c, cd, ab)
+    p.face(b, bd, d, cd, ab)
+    p.face(e, eg, g, gh, ef)
+    p.face(f, fh, h, gh, ef)
+    p.face(a, ae, e, eg, ac)
+    p.face(c, cg, g, eg, ac)
+    p.face(b, bf, f, fh, bd)
+    p.face(d, dh, h, fh, bd)
+
+    return p
 
 def icosahedron():
+    p = polyhedron()
     # The classical easy way to define an icosahedron in Cartesian
     # coordinates is to imagine three interlocking cards along the
     # three axes, each in golden ratio.
     phi = (1.0 + math.sqrt(5))/2.0
 
-    a = vertex(0, -1, -phi)
-    b = vertex(0, -1, +phi)
-    c = vertex(0, +1, -phi)
-    d = vertex(0, +1, +phi)
+    a = p.vertex(0, -1, -phi)
+    b = p.vertex(0, -1, +phi)
+    c = p.vertex(0, +1, -phi)
+    d = p.vertex(0, +1, +phi)
 
-    e = vertex(-1, -phi, 0)
-    f = vertex(-1, +phi, 0)
-    g = vertex(+1, -phi, 0)
-    h = vertex(+1, +phi, 0)
+    e = p.vertex(-1, -phi, 0)
+    f = p.vertex(-1, +phi, 0)
+    g = p.vertex(+1, -phi, 0)
+    h = p.vertex(+1, +phi, 0)
 
-    i = vertex(-phi, 0, -1)
-    j = vertex(+phi, 0, -1)
-    k = vertex(-phi, 0, +1)
-    l = vertex(+phi, 0, +1)
+    i = p.vertex(-phi, 0, -1)
+    j = p.vertex(+phi, 0, -1)
+    k = p.vertex(-phi, 0, +1)
+    l = p.vertex(+phi, 0, +1)
 
     # Now. We have a bunch of faces which use the short edge of one
     # of the cards.
-    face(a, c, i)
-    face(a, c, j)
-    face(b, d, k)
-    face(b, d, l)
-    face(e, g, a)
-    face(e, g, b)
-    face(f, h, c)
-    face(f, h, d)
-    face(i, k, e)
-    face(i, k, f)
-    face(j, l, g)
-    face(j, l, h)
+    p.face(a, c, i)
+    p.face(a, c, j)
+    p.face(b, d, k)
+    p.face(b, d, l)
+    p.face(e, g, a)
+    p.face(e, g, b)
+    p.face(f, h, c)
+    p.face(f, h, d)
+    p.face(i, k, e)
+    p.face(i, k, f)
+    p.face(j, l, g)
+    p.face(j, l, h)
 
     # And then we have eight more faces which each use one vertex
     # from each card.
-    face(a, e, i)
-    face(a, g, j)
-    face(b, e, k)
-    face(b, g, l)
-    face(c, f, i)
-    face(c, h, j)
-    face(d, f, k)
-    face(d, h, l)
+    p.face(a, e, i)
+    p.face(a, g, j)
+    p.face(b, e, k)
+    p.face(b, g, l)
+    p.face(c, f, i)
+    p.face(c, h, j)
+    p.face(d, f, k)
+    p.face(d, h, l)
+
+    return p
+
+def edges(p):
+    # Return a list of all the edges of a polyhedron, in the form
+    # of a list of 2-tuples containing vertex names.
+    elist = []
+    for f in p.flist:
+	vl = p.faces[f]
+	for i in range(len(vl)):
+	    v1, v2 = vl[i-1], vl[i]  # i==0 neatly causes wraparound
+	    if v1 < v2: # only add each edge once
+		elist.append((v1,v2))
+    return elist
+
+class edgedual:
+    # This wrapper class takes as input two functions returning
+    # dual polyhedron objects, and implements a callable object
+    # which returns the edge dual of those polyhedra.
+    def __init__(self, p1, p2):
+	self.p1 = p1
+	self.p2 = p2
+    def __call__(self):
+	p1 = self.p1()
+	p2 = self.p2()
+	pout = polyhedron()
+	# Find the edges of each.
+	e1 = edges(p1)
+	e2 = edges(p2)
+	# For each polyhedron, find the radius vectors from the
+	# origin to the middle of each edge. As a by-produt of this
+	# phase, we also find the scale factor which normalises
+	# each polyhedron to the size where those radii have length
+	# 1.
+	r1 = {}
+	r2 = {}
+	for p, e, r in (p1,e1,r1),(p2,e2,r2):
+	    sl = sn = 0
+	    for v1, v2 in e:
+		x1, y1, z1 = p.vertices[v1]
+		x2, y2, z2 = p.vertices[v2]
+		x, y, z = (x1+x2)/2, (y1+y2)/2, (z1+z2)/2
+		l = math.sqrt(x*x+y*y+z*z)
+		r[(v1,v2)] = x, y, z
+		sl = sl + l
+		sn = sn + 1
+	    r[None] = sn / sl
+	# Now match up the edges between the two polyhedra. As a
+	# test of duality, we expect to end up with an exact
+	# bijection. We test this by finding the matches by
+	# iteration over e2 (so we know everything in e2 maps to
+	# exactly one thing), but storing them in a mapping from e1
+	# to e2, and finally testing that that mapping has the
+	# right size (so we know everything in e1 maps to at least
+	# one thing).
+	emap = {}
+	scale1 = r1[None]
+	scale2 = r2[None]
+	for e in e2:
+	    n = r2[e]
+	    n = (n[0]*scale2, n[1]*scale2, n[2]*scale2)
+	    best = None
+	    bestdist = None
+	    for eprime in e1:
+		nprime = r1[eprime]
+		nprime = (nprime[0]*scale2, nprime[1]*scale2, nprime[2]*scale2)
+		dist = (nprime[0]-n[0])**2 + \
+		(nprime[1]-n[1])**2 + (nprime[2]-n[2])**2
+		if bestdist == None or dist < bestdist:
+		    bestdist = dist
+		    best = eprime
+	    emap[best] = e
+	assert len(emap) == len(e1)
+
+	# Output the edge dual's vertices.
+	vmap1 = {}
+	vmap2 = {}
+	for p, r, vmap in (p1,r1,vmap1),(p2,r2,vmap2):
+	    scale = r[None]
+	    for v in p.vlist:
+		x, y, z = p.vertices[v]
+		vmap[v] = pout.vertex(x*scale, y*scale, z*scale)
+
+	# And output the faces.
+	for e1, e2 in emap.items():
+	    pout.face(vmap1[e1[0]], vmap2[e2[0]], vmap1[e1[1]], vmap2[e2[1]])
+
+	return pout
+
+class output:
+    # This wrapper class takes a function returning a polyhedron
+    # object, and implements a callable object which outputs that
+    # polyhedron to stdout.
+    def __init__(self, fn):
+	self.fn = fn
+    def __call__(self, file=sys.stdout):
+	p = self.fn()
+	p.output(file)
+
+def all():
+    for name, fn in polyhedra.items():
+	if name == "all":
+	    continue
+	file = open(name, "w")
+	fn(file)
+	file.close()
 
 polyhedra = {
-"tetrahedron": tetrahedron,
-"cube": cube,
-"octahedron": octahedron,
-"dodecahedron": dodecahedron,
-"icosahedron": icosahedron,
+"tetrahedron": output(tetrahedron),
+"cube": output(cube),
+"octahedron": output(octahedron),
+"dodecahedron": output(dodecahedron),
+"icosahedron": output(icosahedron),
+"rhombicdodecahedron": output(edgedual(cube, octahedron)),
+"rhombictriacontahedron": output(edgedual(dodecahedron, icosahedron)),
+"all": all,
 }
 
 args = sys.argv[1:]
