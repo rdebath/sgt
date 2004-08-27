09471c3e7f6a5593f22dea30c70443f445af63c5
Simon Tatham <anakin@pobox.com>
2004-08-27 10:52:35 +0000

    Bring my standard Sawfish configuration under version control.
    git-svn-id: svn://svn.tartarus.org/sgt@4503 cda61777-01e9-0310-a592-d414129be87e

---
+++ sawfish.jl
@@ -0,0 +1,94 @@
+;; Simon Tatham's standardised Sawfish setup file.
+;;
+;; This file is best included in your Sawfish configuration by means
+;; of adding a line like this to ~/.sawfish/rc:
+;;
+;;   (load (concat (getenv "HOME") "/src/local/sawfish.jl") nil t t)
+
+(setq ignore-program-positions nil)
+(setq sp-padding 6)
+
+;; On creation of new windows, fiddle with their properties.
+(define (sgt-add-window w)
+  ;; Mozilla 1.6 has a nasty habit of trying to place all its
+  ;; windows at (0,0).
+  (when (equal (caddr (get-x-property w 'WM_CLASS)) "Gecko\0Mozilla-bin\0")
+   (window-put w 'ignore-program-position t)
+  )
+)
+(add-hook 'add-window-hook sgt-add-window)
+
+;; Fun new focus mode. Acts like `enter-only' in _almost_ all cases,
+;; except that when a new window is created _underneath_
+;; the mouse pointer, it does not necessarily gain focus as a
+;; result: if the currently focused window is a PuTTY or pterm, that
+;; window keeps the focus and the new window can be explicitly
+;; focused either by clicking or by moving the mouse out and back in.
+;;
+;; To do this I have to be a bit sneaky, since X doesn't naturally
+;; provide a means of doing this. We first expect to receive a
+;; map-notify for the window, and _then_ later we receive an
+;; enter-notify because the pointer is now inside it. But at the
+;; time of the map-notify we can already tell this is going to
+;; happen by using query-pointer-window.
+;;
+;; So. I start by defining a map-notify hook which notices when a
+;; window is mapped under the pointer, and (if the currently
+;; focused window is one we don't want to take focus away from)
+;; responds by storing that window ID in a global variable. The
+;; actual focus-mode handler will ignore a single enter-notify for
+;; that window ID, _if_ it's the very next enter-notify. (This
+;; ensures that waving the mouse about will clear any incorrect
+;; setting of this variable and avoid any trouble.)
+
+;; This function contains the knowledge of which window types we do
+;; not want to unexpectedly take focus away from.
+(define (sgt-keeps-focus-p w)
+  (or (equal (caddr (get-x-property w 'WM_CLASS))
+              "putty\0Putty\0")
+      (equal (caddr (get-x-property w 'WM_CLASS))
+              "pterm\0Pterm\0")
+  )
+)
+
+;; Hook called when a window is mapped, which stashes the new window
+;; ID so we can remember to ignore a pointer-in event for it later.
+(setq sgt-enter-notify-ignore nil)
+(define (sgt-map-notify w)
+  ;;(print (list 'map-notify w))
+  (when (and (equal w (query-pointer-window))
+         (not (null (input-focus)))
+         (sgt-keeps-focus-p (input-focus)))
+   ;;(print (list 'prepare-to-ignore w))
+   (setq sgt-enter-notify-ignore w)
+  )
+)
+(add-hook 'map-notify-hook sgt-map-notify)
+
+;; The actual focus mode.
+(define-focus-mode 'sgt-enter-only
+    (lambda (w action)
+      ;;(print (list 'enter-only w action 'ignoring sgt-enter-notify-ignore))
+      (case action
+        ((pointer-in)
+         (when (and (window-really-wants-input-p w)
+                    (not (equal sgt-enter-notify-ignore w)))
+           ;;(print (list 'setting-focus-to w))
+           (set-input-focus w))
+           ;;(print 'removing-ignore)
+           (setq sgt-enter-notify-ignore nil))
+        ((focus-in)
+	 (focus-pop-map w))
+	((focus-out add-window)
+	 (unless (or (not (window-really-wants-input-p w))
+		     (eq w (input-focus)))
+	   (focus-push-map w click-to-focus-map)))
+	((before-mode-change)
+	 (focus-pop-map w))
+	((after-mode-change)
+	 (unless (or (not (window-really-wants-input-p w))
+		     (eq w (input-focus)))
+	   (focus-push-map w click-to-focus-map))))))
+
+;; Set it as the default.
+(setq focus-mode 'sgt-enter-only)
