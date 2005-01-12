44944f874197900c4044d1aa8473ccae16178186
Simon Tatham <anakin@pobox.com>
2005-01-12 13:42:56 +0000

    Bring install.sh up to date with current SVN layout, and also add a new piece of code which ensures I have `CheckHostIP no' in my .ssh/config since I've just decided I don't like it.
    git-svn-id: svn://svn.tartarus.org/sgt@5101 cda61777-01e9-0310-a592-d414129be87e

--- local/install.sh
+++ local/install.sh
@@ -1,20 +1,37 @@
 #!/bin/sh
 
 # Set up a Unix account the way I like it.
+# Requires this `local' directory checked out, and also `utils'.
 
-SCRIPTS="addcr findgrep lns mff mfinger mono mtribs multi newscript"
-SCRIPTS="$SCRIPTS remcr rot13 timber"
+SCRIPTS="addcr findgrep mono mtribs newscript remcr rot13 timber"
+
+test -r $HOME/bin || mkdir $HOME/bin
+
+cd $HOME/src/utils
+make -s install-progs PREFIX=$HOME
 
 cd $HOME/src/local
 
-test -r $HOME/bin || mkdir $HOME/bin
+LNS=$HOME/bin/lns
+
 for i in $SCRIPTS; do
-  test -r $HOME/bin/$i || ./lns $i $HOME/bin/$i
+  test -r $HOME/bin/$i || $LNS $i $HOME/bin/$i
 done
 
-test -r $HOME/.jedrc    || ./lns jed.rc $HOME/.jedrc
-test -r $HOME/.screenrc || ./lns screen.rc $HOME/.screenrc
-test -r $HOME/.sigs     || ./lns signature $HOME/.sigs
+test -r $HOME/.jedrc      || $LNS jed.rc $HOME/.jedrc
+test -r $HOME/.screenrc   || $LNS screen.rc $HOME/.screenrc
+test -r $HOME/.sigs       || $LNS signature $HOME/.sigs
+test -d $HOME/.ssh        || (umask 077; mkdir $HOME/.ssh)
+if test -r $HOME/.ssh/config; then
+  if grep -q '^CheckHostIP' $HOME/.ssh/config; then
+    : # do nothing
+  else
+    (echo 'CheckHostIP no'; cat $HOME/.ssh/config) > $HOME/.ssh/config.new && \
+      mv $HOME/.ssh/config.new $HOME/.ssh/config
+  fi
+else
+  echo 'CheckHostIP no' > $HOME/.ssh/config
+fi
 
 test -r $HOME/.bashrc || {
   echo 'source ~/src/local/bash.rc'
