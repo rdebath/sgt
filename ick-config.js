5e6b5860f5e5ceb335f96fe3ccf5a2e8e2125695
Simon Tatham <anakin@pobox.com>
2005-01-20 15:17:45 +0000

    Cope with LJ urls of the form http://<username>.livejournal.com/... .
    git-svn-id: svn://svn.tartarus.org/sgt@5153 cda61777-01e9-0310-a592-d414129be87e

--- ick-config.js
+++ ick-config.js
@@ -8,22 +8,40 @@ function strprefix(str, pfx)
 
 function RewriteURL(url)
 {
+    var style;
+
+    style = false;
+
     if (strprefix(url, "http://www.livejournal.com/users/") ||
         strprefix(url, "http://www.livejournal.com/community/")) {
-	if (!strprefix(url, "http://www.livejournal.com/users/simont/") &&
-	    url.indexOf("?style=") < 0 && url.indexOf("&style=") < 0) {
-	    var hashpos;
-	    var extra;
-	    hashpos = url.indexOf("#");
-	    if (hashpos < 0)
-		hashpos = url.length;
-	    if (url.indexOf("?") < 0)
-		extra = "?style=mine";
-	    else
-		extra = "&style=mine";
-	    url = url.substr(0, hashpos) + extra +
-		url.substring(hashpos, url.length);
-	}
+	if (!strprefix(url, "http://www.livejournal.com/users/simont/"))
+	    style = true;
+    }
+
+    if (strprefix(url, "http://") && url.indexOf(".livejournal.com/") > 0) {
+	var middle, mpos, list;
+	mpos = url.indexOf(".livejournal.com/");
+	middle = url.substr(7, mpos-7);
+        /* list of livejournal.com hostnames that _aren't_ usernames */
+        list = "/pics/userpic/lists/userpic-origin/files/stat/" +
+            "bigip/stat-origin/img/status/mail/post/www/ljdev/" +
+            "userpic-voxel/wwwstat/files-origin/";
+	if (middle.indexOf(".") < 0 && list.indexOf("/"+middle+"/") < 0)
+	    style = true;
+    }
+
+    if (style && url.indexOf("?style=") < 0 && url.indexOf("&style=") < 0) {
+	var hashpos;
+	var extra;
+	hashpos = url.indexOf("#");
+	if (hashpos < 0)
+	    hashpos = url.length;
+	if (url.indexOf("?") < 0)
+	    extra = "?style=mine";
+	else
+	    extra = "&style=mine";
+	url = url.substr(0, hashpos) + extra +
+	    url.substring(hashpos, url.length);
     }
 
     return url;
