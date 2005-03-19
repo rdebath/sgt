24bf5f49e892cf4c03b3b3eb0c543e3de30c25c2
Jacob Nevins <jacobn@chiark.greenend.org.uk>
2005-03-19 02:26:58 +0000

    Try to make our PGP signing more useful:
     * All the PuTTY tools for Windows and Unix now contain the fingerprints of
       the Master Keys. The method for accessing them is crude but universal:
       a new "-pgpfp" command-line option. (Except Unix PuTTYgen, which takes
       "--pgpfp" just to be awkward.)
    
     * Move the key policy discussion from putty-website/keys.html to
       putty/doc/pgpkeys.but, and autogenerate the former from the latter.
       Also tweak the text somewhat and include the fingerprints of the
       Master Keys themselves.
       (I've merged the existing autogeneration scripts into a single new
       one; I've left the old scripts and keys.html around until such time
       as the webmonster reviews the changes and plumbs in the new script;
       he should remove the old files then.)
    
    
    git-svn-id: svn://svn.tartarus.org/sgt@5524 cda61777-01e9-0310-a592-d414129be87e

---
+++ autogen.pl
@@ -0,0 +1,86 @@
+#!/usr/bin/perl
+# usage: autogen.pl <destfile> eg. autogen.pl faq.html
+
+@files = (
+  [ "faq.html",      "AppendixA.html", "FAQ", "PuTTY FAQ" ],
+  [ "feedback.html", "AppendixB.html", "Feedback",
+        "PuTTY Feedback and Bug Reporting" ],
+  [ "keys.html",     "AppendixE.html", "Keys",
+        "PuTTY Download Keys and Signatures" ]
+);
+
+foreach (@files) {
+    ($dstfn, $srcfn, $sect, $title) = @$_ if ($ARGV[0] eq $_->[0]);
+}
+die "must specify a valid page" unless defined($dstfn);
+
+print << "EOF";
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<HTML><HEAD>
+<TITLE>$title</TITLE>
+<link rel="stylesheet" type="text/css" href="sitestyle.css" title="PuTTY Home Page Style">
+</HEAD>
+<BODY>
+<h1 align=center>$title</h1>
+
+<p align=center>
+EOF
+
+print lnk("./", "Home") . " |\n" .
+      lnk("licence.html", "Licence") . " |\n" .
+      lnk("faq.html", "FAQ") . " |\n" .
+      lnk("docs.html", "Docs") . " |\n" .
+      lnk("download.html", "Download") . " |\n" .
+      lnk("keys.html", "Keys") . " |\n" .
+      lnk("links.html", "Links") . "<br>\n" .
+      lnk("mirrors.html", "Mirrors") . " |\n" .
+      lnk("maillist.html", "Updates") . " |\n" .
+      lnk("feedback.html", "Feedback") . " |\n" .
+      lnk("changes.html", "Changes") . " |\n" .
+      lnk("wishlist/", "Wishlist") . " |\n" .
+      lnk("team.html", "Team") . "</p>\n";
+
+sub lnk {
+  ($dst, $capt) = @_;
+  if ($capt eq $sect) {
+    return "<b>$capt</b>";
+  } else {
+    return "<a href=\"$dst\">$capt</a>";
+  }
+}
+
+open FILE, $ENV{"HOME"} . "/pub/putty/doc/" . $srcfn;
+while (<FILE>) {
+  last if /<body>/i;
+}
+# The line after <body> is the navigation links within the Halibut output
+<FILE>;
+
+while (<FILE>) {
+  # The bottom navigation links terminate the file material;
+  # conveniently, they use single quotes in the hrefs instead of
+  # double.
+  last if /^<p><a href='/i;
+  # for each line, we must rewrite hrefs
+  s/<a href="([^"]*)">/'<a href="' . &fix_href($1) . '">'/eg;
+  # don't put in the </body> or </html>, since we're adding text on
+  # the end
+  s/<\/(body|html)>//ig;
+  print;
+}
+
+print << 'EOF';
+
+<br>
+(last modified on <!--LASTMOD-->[insert date here]<!--END-->)
+</BODY></HTML>
+EOF
+
+sub fix_href {
+  local ($_) = @_;
+  s#^\Qhttp://www.chiark.greenend.org.uk/~sgtatham/putty/\E(.+)#$1# or /\// or
+    /^news:/ or /^mailto:/ or
+    /^#/ or s/^\Q$srcfn\E// or
+    s#^#http://www.tartarus.org/~simon/puttydoc\/#;
+  $_;
+}
