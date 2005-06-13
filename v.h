587808494db6df3f2b2f2de3b84f68c5d8f91ec1
Simon Tatham <anakin@pobox.com>
2005-06-13 12:41:55 +0000

    I don't know of any image viewer which isn't at least two of (a) slow, (b) shareware, (c) possessed of various UI annoyances, or (d) no longer conveniently available. (Debian 3.1's placing of eeyes into category (d) was the main trigger for this realisation.) But image libraries are easy to get hold of and make a simple image viewer an astonishingly small project, so here's a first cut at writing my own. I doubt it will ever do much, but what it does do it will do fast and without fuss.
    Currently it only works with GTK 1.2 and gdk-imlib1. The imlib end
    is abstracted away from the main code, so I hope to be able to port
    it to use the incompatible Imlib2 and hence update it to GTK 2.
    
    Working title is `v', but I don't anticipate that that will last
    long if it ever acquires users other than me.
    
    
    git-svn-id: svn://svn.tartarus.org/sgt@5954 cda61777-01e9-0310-a592-d414129be87e

---
+++ v.h
@@ -0,0 +1,43 @@
+/*
+ * v.h: header for my image viewer `v'.
+ */
+
+#ifndef V_V_H
+#define V_V_H
+
+#include <gtk/gtk.h>
+#include <gdk/gdk.h>
+
+#include <stdlib.h> /* for size_t */
+
+/*
+ * v.c
+ */
+void fatal(char *fmt, ...);
+
+/*
+ * malloc.c
+ */
+void *smalloc(size_t size);
+void *srealloc(void *p, size_t size);
+void sfree(void *p);
+char *dupstr(const char *s);
+#define snew(type) \
+    ( (type *) smalloc (sizeof (type)) )
+#define snewn(number, type) \
+    ( (type *) smalloc ((number) * sizeof (type)) )
+#define sresize(array, number, type) \
+    ( (type *) srealloc ((array), (number) * sizeof (type)) )
+
+/*
+ * Image backend files.
+ */
+typedef struct image image;
+void image_init(void);
+image *image_load(char *filename, char **err);
+void image_free(image *img);
+int image_width(image *img);
+int image_height(image *img);
+void image_to_pixmap(image *img, GdkPixmap *pm, int w, int h);
+
+#endif
