e195e3d77e45cb863f6aec81371aad637bda99d9
Simon Tatham <anakin@pobox.com>
2005-06-13 21:15:46 +0000

    Aha, my abstraction layer has rapidly borne fruit! Here's an alternative back end for Imlib2, which means I can now build v as a GTK 2 application. (Imlib2 should also work with GTK 1.2, as far as I'm aware. Imlib1 _only_ works with 1.2.)
    git-svn-id: svn://svn.tartarus.org/sgt@5956 cda61777-01e9-0310-a592-d414129be87e

--- Makefile
+++ Makefile
@@ -1,10 +1,15 @@
 # Makefile for v.
 
-# Currently we require gtk-1.2 because we use gdk-imlib1 which
-# won't work with 2.0. Ideally I'd like to move up to supporting
-# either of that and Imlib2 (which will take some ifdefs or a
-# porting layer, but doesn't otherwise look too hard).
+FLAVOUR = imlib2
+
+ifeq ($(FLAVOUR),imlib1)
+# gdk-imlib1 won't work with 2.0.
 GTK_CONFIG = gtk-config
+LIBS = -lgdk_imlib
+else
+GTK_CONFIG = pkg-config gtk+-2.0 imlib2
+LIBS =#
+endif
 
 CFLAGS = -O2 -Wall -Werror -g -I./ `$(GTK_CONFIG) --cflags`
 LDFLAGS = `$(GTK_CONFIG) --libs`
@@ -14,12 +19,13 @@ LDFLAGS = `$(GTK_CONFIG) --libs`
 
 all: v
 
-V = v.o malloc.o bumf.o imlib1.o
+V = v.o malloc.o bumf.o $(FLAVOUR).o
 v: $(V)
-	$(CC) $(LDFLAGS) -o $@ $(V) -lgdk_imlib
+	$(CC) $(LDFLAGS) -o $@ $(V) $(LIBS)
 
 malloc.o: ./malloc.c ./v.h
 imlib1.o: ./imlib1.c ./v.h
+imlib2.o: ./imlib2.c ./v.h
 v.o: ./v.c ./v.h
 bumf.o: ./bumf.c ./v.h
 
---
+++ imlib2.c
@@ -0,0 +1,84 @@
+/*
+ * Back end for `v' based on imlib2.
+ */
+
+#include "v.h"
+
+#include <X11/X.h>
+#include <X11/Xlib.h>
+#include <X11/Xutil.h>
+
+#include <Imlib2.h>
+#include <gdk/gdkx.h>
+
+struct image {
+    Imlib_Image image;
+};
+
+void image_init(void)
+{
+}
+
+image *image_load(char *filename, char **err)
+{
+    Imlib_Image im;
+    image *ret;
+
+    im = imlib_load_image(filename);
+    if (!im) {
+	if (err)
+	    *err = "imlib_load_image returned failure";
+	return NULL;
+    } else {
+	ret = snew(image);
+	ret->image = im;
+	return ret;
+    }
+}
+
+void image_free(image *img)
+{
+    imlib_context_set_image(img->image);
+    imlib_free_image();
+    sfree(img);
+}
+
+int image_width(image *img)
+{
+    imlib_context_set_image(img->image);
+    return imlib_image_get_width();
+}
+
+int image_height(image *img)
+{
+    imlib_context_set_image(img->image);
+    return imlib_image_get_height();
+}
+
+void image_to_pixmap(image *img, GdkPixmap *pm, int w, int h)
+{
+    int realw, realh;
+    int need_free;
+
+    imlib_context_set_image(img->image);
+    realw = imlib_image_get_width();
+    realh = imlib_image_get_height();
+    if (w != realw || h != realh) {
+	Imlib_Image newimg;
+
+	newimg = imlib_create_cropped_scaled_image(0, 0, realw, realh, w, h);
+	imlib_context_set_image(newimg);
+	need_free = TRUE;
+    } else
+	need_free = FALSE;
+
+    imlib_context_set_display(GDK_WINDOW_XDISPLAY(pm));
+    imlib_context_set_visual(GDK_VISUAL_XVISUAL(gdk_visual_get_system()));
+    imlib_context_set_colormap
+	(GDK_COLORMAP_XCOLORMAP(gdk_colormap_get_system()));
+    imlib_context_set_drawable(GDK_WINDOW_XWINDOW(pm));
+    imlib_render_image_on_drawable(0, 0);
+
+    if (need_free)
+	imlib_free_image();
+}
