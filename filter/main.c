5b811f56e291a7a5c616efe77e1f59bc0faf6e6b
Simon Tatham <anakin@pobox.com>
2006-05-13 09:06:00 +0000

    Rework the main program for these filters so that it does simultaneous select() between its input _and_ output fds. It should now never deadlock.
    git-svn-id: svn://svn.tartarus.org/sgt@6680 cda61777-01e9-0310-a592-d414129be87e

--- filter/main.c
+++ filter/main.c
@@ -11,15 +11,124 @@
 #include <termios.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <math.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 #include "filter.h"
 
+#define min(x,y) ( (x)<(y)?(x):(y) )
+#define max(x,y) ( (x)>(y)?(x):(y) )
+
+/* ----------------------------------------------------------------------
+ * Chunk of code lifted from PuTTY's misc.c to manage buffers of
+ * data to be written to an fd.
+ */
+
+#define BUFFER_GRANULE  512
+
+typedef struct bufchain_tag {
+    struct bufchain_granule *head, *tail;
+    int buffersize;		       /* current amount of buffered data */
+} bufchain;
+struct bufchain_granule {
+    struct bufchain_granule *next;
+    int buflen, bufpos;
+    char buf[BUFFER_GRANULE];
+};
+
+static void bufchain_init(bufchain *ch)
+{
+    ch->head = ch->tail = NULL;
+    ch->buffersize = 0;
+}
+
+static int bufchain_size(bufchain *ch)
+{
+    return ch->buffersize;
+}
+
+static void bufchain_add(bufchain *ch, const void *data, int len)
+{
+    const char *buf = (const char *)data;
+
+    if (len == 0) return;
+
+    ch->buffersize += len;
+
+    if (ch->tail && ch->tail->buflen < BUFFER_GRANULE) {
+	int copylen = min(len, BUFFER_GRANULE - ch->tail->buflen);
+	memcpy(ch->tail->buf + ch->tail->buflen, buf, copylen);
+	buf += copylen;
+	len -= copylen;
+	ch->tail->buflen += copylen;
+    }
+    while (len > 0) {
+	int grainlen = min(len, BUFFER_GRANULE);
+	struct bufchain_granule *newbuf;
+	newbuf = (struct bufchain_granule *)
+	    malloc(sizeof(struct bufchain_granule));
+	if (!newbuf) {
+	    fprintf(stderr, "filter: out of memory\n");
+	    exit(1);
+	}
+	newbuf->bufpos = 0;
+	newbuf->buflen = grainlen;
+	memcpy(newbuf->buf, buf, grainlen);
+	buf += grainlen;
+	len -= grainlen;
+	if (ch->tail)
+	    ch->tail->next = newbuf;
+	else
+	    ch->head = ch->tail = newbuf;
+	newbuf->next = NULL;
+	ch->tail = newbuf;
+    }
+}
+
+static void bufchain_consume(bufchain *ch, int len)
+{
+    struct bufchain_granule *tmp;
+
+    assert(ch->buffersize >= len);
+    while (len > 0) {
+	int remlen = len;
+	assert(ch->head != NULL);
+	if (remlen >= ch->head->buflen - ch->head->bufpos) {
+	    remlen = ch->head->buflen - ch->head->bufpos;
+	    tmp = ch->head;
+	    ch->head = tmp->next;
+	    free(tmp);
+	    if (!ch->head)
+		ch->tail = NULL;
+	} else
+	    ch->head->bufpos += remlen;
+	ch->buffersize -= remlen;
+	len -= remlen;
+    }
+}
+
+static void bufchain_prefix(bufchain *ch, void **data, int *len)
+{
+    *len = ch->head->buflen - ch->head->bufpos;
+    *data = ch->head->buf + ch->head->bufpos;
+}
+
+/* ----------------------------------------------------------------------
+ * End of bufchain stuff. Main program begins.
+ */
+
+#define LOCALBUF_LIMIT 65536
+
 sig_atomic_t exitcode = -1;
+pid_t childpid = -1;
 
 void sigchld(int sig)
 {
-    int pid;
-    exitcode = wait(&pid);	       /* reap the exit code */
+    int pid, code;
+    pid = wait(&code);		       /* reap the exit code */
+    if (pid == childpid)
+	exitcode = code;
 }
 
 struct termios oldattrs, newattrs;
@@ -65,8 +174,10 @@ int main(int argc, char **argv)
     char ptyname[FILENAME_MAX];
     tstate *state;
     struct mainopts opts;
+    bufchain tochild, tostdout;
+    int tochild_active, tostdout_active, fromstdin_active, fromchild_active;
 
-    opts.pipe = TRUE;
+    opts.pipe = FALSE;
 
     state = tstate_init();
 
@@ -125,8 +236,10 @@ int main(int argc, char **argv)
 			    fprintf(stderr, "option -%c expects an"
 				    " argument\n", c);
 			    errors = 1;
+			    q = NULL;
 			}
-			ret = option(&opts, state, c, NULL, q);
+			if (q)
+			    ret = option(&opts, state, c, NULL, q);
 		    }
 
 		    if (ret == OPT_UNKNOWN) {
@@ -174,6 +287,10 @@ int main(int argc, char **argv)
 	}
     }
 
+    bufchain_init(&tochild);
+    bufchain_init(&tostdout);
+    tochild_active = tostdout_active = TRUE;
+    fromchild_active = fromstdin_active = TRUE;
     tstate_ready(state, &iwait, &owait);
 
     /*
@@ -227,6 +344,8 @@ int main(int argc, char **argv)
      */
     close(slaver);
     close(slavew);
+    childpid = pid;
+    signal(SIGCHLD, sigchld);
 
     if (!opts.pipe) {
 	tcgetattr(0, &oldattrs);
@@ -239,47 +358,65 @@ int main(int argc, char **argv)
     }
 
     while (1) {
-	fd_set rset;
+	fd_set rset, wset;
 	char buf[65536];
-	int ret;
+	int maxfd, ret;
 	double wait;
-	int itimeout, otimeout;
+	int itimeout, otimeout, used_iwait, used_owait;
 	struct timeval tv, *ptv;
 
-	if (iwait && owait)
-	    wait = (iwait < owait ? iwait : owait);
-	else if (iwait)
-	    wait = iwait;
-	else if (owait)
-	    wait = owait;
-	else
-	    wait = 0.0;
+	FD_ZERO(&rset);
+	FD_ZERO(&wset);
+	maxfd = 0;
+	wait = HUGE_VAL;
+	used_iwait = used_owait = FALSE;
 
-	if (wait) {
+	if (tochild_active && bufchain_size(&tochild)) {
+	    FD_SET(masterw, &wset);
+	    maxfd = max(masterw+1, maxfd);
+	}
+	if (tostdout_active && bufchain_size(&tostdout)) {
+	    FD_SET(1, &wset);
+	    maxfd = max(1+1, maxfd);
+	}
+	if (fromstdin_active && bufchain_size(&tochild) < LOCALBUF_LIMIT) {
+	    FD_SET(0, &rset);
+	    maxfd = max(0+1, maxfd);
+	    if (iwait) {
+		used_iwait = TRUE;
+		wait = min(wait, iwait);
+	    }
+	}
+	if (fromchild_active && bufchain_size(&tostdout) < LOCALBUF_LIMIT) {
+	    FD_SET(masterr, &rset);
+	    maxfd = max(masterr+1, maxfd);
+	    if (owait) {
+		used_owait = TRUE;
+		wait = min(wait, owait);
+	    }
+	}
+
+	if (wait == HUGE_VAL) {
+	    ptv = NULL;
+	} else {
 	    ptv = &tv;
 	    tv.tv_sec = (int)wait;
 	    tv.tv_usec = (int)1000000 * (wait - tv.tv_sec);
-	} else {
-	    ptv = NULL;
 	}
 
-	FD_ZERO(&rset);
-	FD_SET(masterr, &rset);
-	FD_SET(0, &rset);
-
-	ret = select(masterr+1, &rset, NULL, NULL, ptv);
+	ret = select(maxfd, &rset, &wset, NULL, ptv);
 
 	itimeout = otimeout = FALSE;
 	if (ret == 0) {
 	    double left = (ret == 0 ? 0.0 : tv.tv_sec + tv.tv_usec/1000000.0);
 	    wait -= left;
-	    if (iwait) {
+	    if (iwait && used_iwait) {
 		iwait -= wait;
 		assert(iwait >= 0);
 		if (!iwait)
 		    itimeout = TRUE;
 	    }
-	    if (owait) {
+	    if (owait && used_owait) {
 		owait -= wait;
 		assert(owait >= 0);
 		if (!owait)
@@ -298,17 +435,32 @@ int main(int argc, char **argv)
 
 	    if (FD_ISSET(masterr, &rset)) {
 		ret = read(masterr, buf, sizeof(buf));
-		if (ret <= 0)
-		    break;
+		if (ret <= 0) {
+		    /*
+		     * EIO from a pty master just means end of
+		     * file, annoyingly. Why can't it report
+		     * ordinary EOF?
+		     */
+		    if (!opts.pipe && errno == EIO)
+			ret = 0;
+		    if (ret < 0) {
+			perror("child process: read");
+		    }
+		    close(masterr);
+		    fromchild_active = FALSE;
+		    ret = 0;
+		}
 	    } else
 		ret = 0;
 
-	    translated = translate(state, buf, ret, &translen, &owait, 0);
+	    if (ret || otimeout) {
+		translated = translate(state, buf, ret, &translen, &owait, 0);
 
-	    if (translen > 0)
-		write(1, translated, translen);
+		if (translen > 0)
+		    bufchain_add(&tostdout, translated, translen);
 
-	    free(translated);
+		free(translated);
+	    }
 	}
 	if (FD_ISSET(0, &rset) || itimeout) {
 	    char *translated;
@@ -316,17 +468,99 @@ int main(int argc, char **argv)
 
 	    if (FD_ISSET(0, &rset)) {
 		ret = read(0, buf, sizeof(buf));
-		if (ret <= 0)
-		    break;
+		if (ret <= 0) {
+		    if (ret < 0) {
+			perror("stdin: read");
+		    }
+		    close(0);
+		    fromstdin_active = FALSE;
+		    ret = 0;
+		}
 	    } else
 		ret = 0;
 
-	    translated = translate(state, buf, ret, &translen, &iwait, 1);
+	    if (ret || itimeout) {
+		translated = translate(state, buf, ret, &translen, &iwait, 1);
 
-	    if (translen)
-		write(masterw, translated, translen);
+		if (translen)
+		    bufchain_add(&tochild, translated, translen);
 
-	    free(translated);
+		free(translated);
+	    }
+	}
+	if (FD_ISSET(1, &wset)) {
+	    void *data;
+	    int len, ret;
+	    bufchain_prefix(&tostdout, &data, &len);
+	    if ((ret = write(1, data, len)) < 0) {
+		perror("stdout: write");
+		close(1);
+		close(masterr);
+		tostdout_active = fromchild_active = FALSE;
+	    } else
+		bufchain_consume(&tostdout, ret);
+	}
+	if (FD_ISSET(masterw, &wset)) {
+	    void *data;
+	    int len;
+	    bufchain_prefix(&tochild, &data, &len);
+	    if ((ret = write(masterw, data, len)) < 0) {
+		perror("child process: write");
+		close(0);
+		close(masterw);
+		tochild_active = fromstdin_active = FALSE;
+	    } else
+		bufchain_consume(&tochild, ret);
+	}
+
+	/*
+	 * If there can be no further data from a direction (the
+	 * input fd has been closed and the buffered data is used
+	 * up) but its output fd is still open, close it.
+	 */
+	if (!fromstdin_active && !bufchain_size(&tochild) && tochild_active) {
+	    tochild_active = FALSE;
+	    close(masterw);
+	}
+	if (!fromchild_active && !bufchain_size(&tostdout) && tostdout_active){
+	    tostdout_active = FALSE;
+	    close(1);
+	}
+
+	/*
+	 * Termination condition with pipes is that there's still
+	 * data flowing in at least one direction.
+	 * 
+	 * Termination condition for a pty-based run is that the
+	 * child process hasn't yet terminated and/or there is
+	 * still buffered data to send.
+	 */
+	if (opts.pipe) {
+	    /*
+	     * The condition `there is data flowing in a direction'
+	     * unpacks as: there is at least potentially data left
+	     * to be written (i.e. EITHER the input side of that
+	     * direction is still open OR we have some buffered
+	     * data left from before it closed) AND we are able to
+	     * write it (the output side is still active).
+	     */
+	    if (tochild_active &&
+		(fromstdin_active || bufchain_size(&tochild)))
+		/* stdin -> child direction is still active */;
+	    else if (tostdout_active &&
+		     (fromchild_active || bufchain_size(&tostdout)))
+		/* child -> stdout direction is still active */;
+	    else
+		break;		       /* neither is active any more */
+	} else {
+	    if (exitcode < 0)
+		/* process is still active */;
+	    else if (tochild_active && bufchain_size(&tochild))
+		/* data still to be sent to child's children */;
+	    else if (tostdout_active && bufchain_size(&tostdout))
+		/* data still to be sent to stdout */;
+	    else
+		break;		       /* terminate */
 	}
     }
 
@@ -334,11 +568,13 @@ int main(int argc, char **argv)
     close(masterr);
 
     if (exitcode < 0) {
-	int pid;
-	exitcode = wait(&pid);
+	int pid, code;
+	pid = wait(&code);
+	exitcode = code;
     }
 
     tstate_done(state);
 
-    return exitcode;
+    return (WIFEXITED(exitcode) ? WEXITSTATUS(exitcode) :
+	    128 | WTERMSIG(exitcode));
 }
