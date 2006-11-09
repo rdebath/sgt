d6fffcba16961057b01873b36de6e9ca24845564
Simon Tatham <anakin@pobox.com>
2006-11-09 12:07:51 +0000

    And a few things I've had lying around on stormhawk as well.
    git-svn-id: svn://svn.tartarus.org/sgt@6898 cda61777-01e9-0310-a592-d414129be87e

---
+++ perm.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+# Routine to break a permutation up into cycles.
+#
+# To use:
+#
+#     $ python -i perm.py
+#     >>> cycles(lambda x: (2*x) % 13, range(13))
+#     [[0], [1, 2, 4, 8, 3, 6, 12, 11, 9, 5, 10, 7]]
+#     >>> cycles(lambda x: (3*x) % 13, range(13))
+#     [[0], [1, 3, 9], [2, 6, 5], [4, 12, 10], [7, 8, 11]]
+
+def cycles(f, l):
+    ret = []
+    l = list(l)
+
+    while len(l) > 0:
+	x = l[0]
+	del l[0]
+	this = [x]
+	y = x
+	while 1:
+	    y = f(y)
+	    if y == x:
+		break
+	    assert y in l
+	    l.remove(y)
+	    this.append(y)
+	ret.append(this)
+
+    return ret
