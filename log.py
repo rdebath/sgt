2c90702ebd536d8ba3bf2c7ff0990e301223a5f1
Simon Tatham <anakin@pobox.com>
2007-02-04 11:02:52 +0000

    My all-singing, all-dancing new build system. Tentatively called `bob', at Gareth's suggestion, although I'm prepared to rename it in a hurry if that turns out to be a bad idea.
    git-svn-id: svn://svn.tartarus.org/sgt@7203 cda61777-01e9-0310-a592-d414129be87e

---
+++ log.py
@@ -0,0 +1,28 @@
+# Logging module for my build scripts.
+
+import sys
+
+logfp = None
+logcfg = None
+
+def init(fp, cfg):
+    global logfp
+    global logcfg
+    logfp = fp
+    logcfg = cfg
+
+def internal_log(msg):
+    logfp.write(msg + "\n")
+    logfp.flush()
+    if logcfg.verbose:
+	sys.stdout.write(msg + "\n")
+	sys.stdout.flush()
+
+def logmsg(s):
+    internal_log("* " + s)
+
+def logscript(s):
+    internal_log("> " + s)
+
+def logoutput(s):
+    internal_log("| " + s)
