b33bd8aec55f85e0b23abcf3ce6a90ca9b8d7bd4
Simon Tatham <anakin@pobox.com>
2007-05-17 17:25:00 +0000

    Export the other C99 functions which are missing from Python proper.
    git-svn-id: svn://svn.tartarus.org/sgt@7596 cda61777-01e9-0310-a592-d414129be87e

--- misc/libcode/pymaths.c
+++ misc/libcode/pymaths.c
@@ -1,6 +1,8 @@
 /*
  * Native-code Python module which provides access from Python to
- * maths library functions I've written in C.
+ * maths library functions I've written in C, and (while I'm at it)
+ * to C99-provided maths library functions which Python's own
+ * `math' module doesn't bother with.
  * 
  * Compile with:
  * 
@@ -19,8 +21,8 @@
 #include "lambertw.c"
 #include "normal.c"
 
-static PyObject *python_wrapper(PyObject *self, PyObject *args,
-				double (*func)(double))
+static PyObject *python_wrapper1(PyObject *self, PyObject *args,
+				 double (*func)(double))
 {
     double d;
 
@@ -31,52 +33,71 @@ static PyObject *python_wrapper(PyObject *self, PyObject *args,
     if (errno == EDOM) {
 	PyErr_SetString(PyExc_ValueError, "math domain error");
 	return NULL;
+    } else if (errno == ERANGE) {
+	PyErr_SetString(PyExc_OverflowError, "math range error");
+	return NULL;
     }
     return Py_BuildValue("d", d);
 }
 
-#define WRAP(fn) \
+static PyObject *python_wrapper2(PyObject *self, PyObject *args,
+				double (*func)(double, double))
+{
+    double d1, d2;
+
+    if (!PyArg_ParseTuple(args, "dd", &d1, &d2))
+	return NULL;
+    errno = 0;
+    d1 = func(d1, d2);
+    if (errno == EDOM) {
+	PyErr_SetString(PyExc_ValueError, "math domain error");
+	return NULL;
+    } else if (errno == ERANGE) {
+	PyErr_SetString(PyExc_OverflowError, "math range error");
+	return NULL;
+    }
+    return Py_BuildValue("d", d1);
+}
+
+#define FNLIST(X,X2) \
+    /* My functions */ \
+    X(W, "The principal branch of the Lambert W function on the real\n" \
+          "interval (-1/e,infinity).") \
+    X(Wn, "The negative branch of the Lambert W function on the real\n" \
+          "interval (-1/e,0).") \
+    X(inverf, "The inverse of the error function.") \
+    X(inverfc, "The inverse of the complementary error function.") \
+    X(Phi, "The cumulative probability function of the standard normal\n" \
+          "distribution.") \
+    X(Phi0, "The cumulative probability function of the standard normal\n" \
+          "distribution, integrated from zero rather than infinity.") \
+    X(invPhi, "The inverse of the cumulative probability function of the\n" \
+          "standard normal distribution.") \
+    X(invPhi0, "The inverse of the cumulative probability function of the\n" \
+          "standard normal distribution integrated from zero.") \
+    /* C99 functions */ \
+    X(erf, "The error function.") \
+    X(erfc, "The complementary error function.") \
+    X(tgamma, "The gamma function.") \
+    X(lgamma, "The logarithm of the (absolute value of the) gamma function.") \
+    X(exp2, "2^x.") \
+    X(log2, "Logarithm to base 2.") \
+    X(expm1, "exp(x)-1.") \
+    X(log1p, "log(1+x).") \
+    X(cbrt, "Cube root.") \
+    X2(hypot, "Hypotenuse function sqrt(x^2+y^2).")
+
+#define WRAPPER1(fn,desc) \
     static PyObject *python_ ## fn(PyObject *self, PyObject *args) \
-	    { return python_wrapper(self, args, fn); }
+	    { return python_wrapper1(self, args, fn); }
+#define WRAPPER2(fn,desc) \
+    static PyObject *python_ ## fn(PyObject *self, PyObject *args) \
+	    { return python_wrapper2(self, args, fn); }
+FNLIST(WRAPPER1,WRAPPER2)
 
-WRAP(W)
-WRAP(Wn)
-WRAP(erf)			       /* standard Python maths module */
-WRAP(erfc)			       /* doesn't provide these, so let's */
-WRAP(inverf)
-WRAP(inverfc)
-WRAP(Phi)
-WRAP(Phi0)
-WRAP(invPhi)
-WRAP(invPhi0)
-
+#define METHOD(fn,desc) { #fn, python_ ## fn, METH_VARARGS, desc },
 static PyMethodDef pymaths_methods[] = {
-    {"W", python_W, METH_VARARGS,
-    "The principal branch of the Lambert W function on the real\n"
-    "interval (-1/e,infinity)."},
-    {"Wn", python_Wn, METH_VARARGS,
-    "The negative branch of the Lambert W function on the real\n"
-    "interval (-1/e,0)."},
-    {"erf", python_erf, METH_VARARGS,
-    "The error function."},
-    {"erfc", python_erfc, METH_VARARGS,
-    "The complementary error function."},
-    {"inverf", python_inverf, METH_VARARGS,
-    "The inverse of the error function."},
-    {"inverfc", python_inverfc, METH_VARARGS,
-    "The inverse of the complementary error function."},
-    {"Phi", python_Phi, METH_VARARGS,
-    "The cumulative probability function of the standard normal\n"
-    "distribution."},
-    {"Phi0", python_Phi0, METH_VARARGS,
-    "The cumulative probability function of the standard normal\n"
-    "distribution, integrated from zero rather than infinity."},
-    {"invPhi", python_invPhi, METH_VARARGS,
-    "The inverse of the cumulative probability function of the\n"
-    "standard normal distribution."},
-    {"invPhi0", python_invPhi0, METH_VARARGS,
-    "The inverse of the cumulative probability function of the\n"
-    "standard normal distribution integrated from zero."},
+    FNLIST(METHOD,METHOD)
     {NULL, NULL, 0, NULL}
 };
 
