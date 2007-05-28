a91df562ab663966da2de9cff9c5b31ba4b21b44
Simon Tatham <anakin@pobox.com>
2007-05-28 09:02:30 +0000

    Introduce ability to link against GMP.
    git-svn-id: svn://svn.tartarus.org/sgt@7608 cda61777-01e9-0310-a592-d414129be87e

--- puzzles/rootsum.c
+++ puzzles/rootsum.c
@@ -8,6 +8,7 @@
 
 /*
  * gcc -I ../../library -o rootsum rootsum.c ../../library/tree234.c -lm
+ * gcc -DGMP -I ../../library -o rootsum rootsum.c ../../library/tree234.c -lm -lgmp
  */
 
 #include <stdio.h>
@@ -16,13 +17,48 @@
 #include <ctype.h>
 #include "tree234.h"
 
+#ifdef GMP
+#include "gmp.h"
+#define DOUBLE mpf_t
+#define SQRT(x,y) mpf_sqrt(x,y)
+#define SET(x,y) mpf_set(x,y)
+#define INIT(x) mpf_init(x)
+#define CLEAR(x) mpf_clear(x)
+#define SET_UI(x,y) mpf_set_ui(x,y)
+#define MODF(x,i,y) (mpf_floor(i,y), mpf_sub(x,y,i))
+#define MUL(r,x,y) mpf_mul(r,x,y)
+#define SUB(r,x,y) mpf_sub(r,x,y)
+#define ADD(r,x,y) mpf_add(r,x,y)
+#define CMP_SI(x,r,y) (mpf_cmp_si(x,y) r 0)
+#define CMP(x,r,y) (mpf_cmp(x,y) r 0)
+#define TO_DOUBLE(x) mpf_get_d(x)
+#define TO_INT(x) mpf_get_si(x)
+#define SETPREC(x) mpf_set_default_prec(x)
+#else
+#define DOUBLE double
+#define SQRT(x,y) ((x) = sqrt((y)))
+#define SET(x,y) ((x) = (y))
+#define INIT(x) ((void)0)
+#define CLEAR(x) ((void)0)
+#define SET_UI(x,y) ((x)=(y))
+#define MODF(x,i,y) ((x) = modf(y,&(i)))
+#define MUL(r,x,y) ((r)=(x)*(y))
+#define SUB(r,x,y) ((r)=(x)-(y))
+#define ADD(r,x,y) ((r)=(x)+(y))
+#define CMP_SI(x,r,y) ((x) r (y))
+#define CMP(x,r,y) ((x) r (y))
+#define TO_DOUBLE(x) (x)
+#define TO_INT(x) ((int)(x))
+#define SETPREC(x) ((void)0)
+#endif
+
 struct root {
     int n;
-    double root;
-    double fracpart;
+    DOUBLE root;
+    DOUBLE fracpart;
 };
 
-double best = 1.0;
+DOUBLE best;
 
 enum { BOTH, POSITIVE, NEGATIVE } mode = BOTH;
 
@@ -30,9 +66,9 @@ int fraccmp(void *av, void *bv)
 {
     struct root *a = (struct root *)av;
     struct root *b = (struct root *)bv;
-    if (a->fracpart < b->fracpart)
+    if (CMP(a->fracpart,<,b->fracpart))
 	return -1;
-    else if (a->fracpart > b->fracpart)
+    else if (CMP(a->fracpart,>,b->fracpart))
 	return +1;
     else if (a->n < b->n)
 	return -1;
@@ -44,11 +80,11 @@ int fraccmp(void *av, void *bv)
 
 int fracsearch(void *av, void *bv)
 {
-    double a = *(double *)av;
+    DOUBLE *a = (DOUBLE *)av;
     struct root *b = (struct root *)bv;
-    if (a < b->fracpart)
+    if (CMP(a[0],<,b->fracpart))
 	return -1;
-    else if (a > b->fracpart)
+    else if (CMP(a[0],>,b->fracpart))
 	return +1;
     else
 	return 0;
@@ -71,45 +107,61 @@ tree234 *byn, *byfrac;
 struct root *compute_root(int n)
 {
     struct root *r = malloc(sizeof(struct root));
-    double intpart;
+    DOUBLE tmp;
+    int rt;
+
+    INIT(tmp);
 
     r->n = n;
-    r->root = sqrt(n);
-    r->fracpart = modf(r->root, &intpart);
+    INIT(r->root);
+    SET_UI(tmp, n);
+    SQRT(r->root, tmp);
+    INIT(r->fracpart);
+    MODF(r->fracpart, tmp, r->root);
+    rt = TO_INT(tmp);
 
-    if ((int)intpart * (int)intpart == n) {
+    if (rt * rt == n) {
 	/*
 	 * Square integer. Ignore.
 	 */
 	free(r);
+	CLEAR(tmp);
 	return NULL;
     }
 
     add234(byfrac, r);
     add234(byn, r);
 
+    CLEAR(tmp);
+
     return r;
 }
 
-void candidate(struct root **roots, int n, double x)
+void candidate(struct root **roots, int n, DOUBLE x)
 {
-    double y;
+    DOUBLE y;
     int i;
 
-    x = modf(x, &y);
-    y = 1.0 - x;
+    INIT(y);
+    MODF(x,y,x);
+    SET_UI(y,1);
+    SUB(y,y,x);
+
     if (mode == POSITIVE)
 	/* leave x as it is */;
     else if (mode == NEGATIVE)
-	x = y;
-    else if (x > y)
-	x = y;
+	SET(x,y);
+    else if (CMP(x,>,y))
+	SET(x,y);
 
-    if (best > x) {
-	double total = roots[0]->root;
+    if (CMP(best,>,x)) {
+	DOUBLE total;
 	int done0 = 0;
 
-	best = x;
+	INIT(total);
+	SET(total, roots[0]->root);
+
+	SET(best, x);
 
 	/*
 	 * Output our new best result. We tweak this slightly to
@@ -124,25 +176,36 @@ void candidate(struct root **roots, int n, double x)
 	    if (i == n)
 		break;
 	    printf("%d ", roots[i]->n);
-	    total += roots[i]->root;
+	    ADD(total, total, roots[i]->root);
 	}
-	printf("%.17g %.17g\n", total, x);
+	printf("%.17g %.17g\n", TO_DOUBLE(total), TO_DOUBLE(x));
+
+	CLEAR(total);
     }
+
+    CLEAR(y);
 }
 
-void search(struct root **roots, int i, int n, double x)
+void search(struct root **roots, int i, int n, DOUBLE x)
 {
     struct root *r;
 
     if (i > 0) {
 	int j;
+	DOUBLE newx;
+
+	INIT(newx);
+
 	for (j = 0;
 	     (r = (struct root *)index234(byn, j)) != NULL &&
 	     r->n <= roots[i+1]->n;
 	     j++) {
 	    roots[i] = r;
-	    search(roots, i-1, n, x + r->fracpart);
+	    ADD(newx, x, r->fracpart);
+	    search(roots, i-1, n, newx);
 	}
+
+	CLEAR(newx);
     } else {
 	/*
 	 * We have all but one of our roots, so we know what we'd
@@ -152,9 +215,13 @@ void search(struct root **roots, int i, int n, double x)
 	 * side).
 	 */
 	int pos;
-	double target, intpart;
+	DOUBLE target, intpart;
 
-	target = modf(n - x, &intpart);
+	INIT(target);
+	INIT(intpart);
+	SET_UI(target, n);
+	SUB(target, target, x);
+	MODF(target, intpart, target);
 	r = (struct root *)
 	    findrelpos234(byfrac, &target, fracsearch, REL234_GE, &pos);
 
@@ -181,9 +248,14 @@ void search(struct root **roots, int i, int n, double x)
 	    pos = count234(byfrac);
 	}
 	roots[i] = r;
-	candidate(roots, n, x + r->fracpart);
+	ADD(target, x, r->fracpart);
+	candidate(roots, n, target);
 	roots[i] = r = index234(byfrac, pos-1);
-	candidate(roots, n, x + r->fracpart);
+	ADD(target, x, r->fracpart);
+	candidate(roots, n, target);
+
+	CLEAR(target);
+	CLEAR(intpart);
     }
 }
 
@@ -193,6 +265,11 @@ int main(int argc, char **argv)
     int maxn;
     struct root *r;
     struct root **roots;
+
+    SETPREC(256);
+
+    INIT(best);
+    SET_UI(best, 1);
 
     byn = newtree234(ncmp);
     byfrac = newtree234(fraccmp);
