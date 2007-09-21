a05ad535a309c4713396970af327c92188039321
Simon Tatham <anakin@pobox.com>
2007-09-21 07:48:08 +0000

    I've had this lying around for a while and should probably check it in.
    git-svn-id: svn://svn.tartarus.org/sgt@7727 cda61777-01e9-0310-a592-d414129be87e

---
+++ waitforit.pl
@@ -0,0 +1,36 @@
+#!/usr/bin/perl
+
+use Time::Local;
+
+$date = shift @ARGV;
+$time = shift @ARGV;
+
+if (!defined $date || !defined $time) {
+    die "usage: waitforit.pl YYYY-MM-DD HH:MM[:SS] <command> <args>\n";
+}
+
+$date =~ /^(\d+)-(\d+)-(\d+)$/ or do {
+    die "date was not in YYYY-MM-DD format\n";
+};
+($year,$mon,$day) = ($1,$2,$3);
+
+$time =~ /^(\d+):(\d+)(:(\d+))?$/ or do {
+    die "time was not in HH:MM[:SS] format\n";
+};
+($hour,$min,$sec) = ($1,$2,$4);
+
+# adjust month by 1 because timelocal() expects jan=0 dec=11
+$time = timelocal($sec,$min,$hour,$day,$mon-1,$year);
+
+if (time > $time) {
+    warn "warning: selected time is already in the past\n";
+}
+
+# Wait for that time. (Loop round out of sheer paranoia!)
+while (($now = time) < $time) {
+    $seconds = $time - $now;
+    sleep $seconds;
+}
+
+# Now do the thing.
+exec @ARGV;
