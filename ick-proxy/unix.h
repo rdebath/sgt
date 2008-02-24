bff552096a7c9a517257e69720cb2c132749aec9
Simon Tatham <anakin@pobox.com>
2008-02-24 12:51:31 +0000

    Support re-reading of configuration at run time in Unix and OS X clients (the former by the traditional SIGHUP, the latter by a dock menu option).
    git-svn-id: svn://svn.tartarus.org/sgt@7885 cda61777-01e9-0310-a592-d414129be87e

--- ick-proxy/unix.h
+++ ick-proxy/unix.h
@@ -5,8 +5,10 @@
 #ifndef ICK_PROXY_UNIX_H
 #define ICK_PROXY_UNIX_H
 
+int configure_single_user(void);
+
 int uxmain(int multiuser, int port, char *dropprivuser, char **singleusercmd,
 	   char *oscript, char *oinpac, char *ooutpac, int clientfd,
-	   int (*clientfdread)(void));
+	   int (*clientfdread)(int fd));
 
 #endif /* ICK_PROXY_UNIX_H */
