fee5464cf02c08bb96a89402f4a3f5ccba1fd40c
Simon Tatham <anakin@pobox.com>
2008-07-26 08:44:41 +0000

    Add a big comment describing smoothsort.
    git-svn-id: svn://svn.tartarus.org/sgt@8127 cda61777-01e9-0310-a592-d414129be87e

--- libcode/smoothsort.c
+++ libcode/smoothsort.c
@@ -2,6 +2,186 @@
  * smoothsort.c: Implementation of Edsger Dijkstra's "smoothsort".
  */
 
+/*
+ * First, an explanation of the algorithm. Dijkstra's own
+ * explanation is not very clear (in my opinion), so I'll write my
+ * own.
+ * 
+ * Smoothsort is structurally similar to heapsort. It involves
+ * building a heap-like structure (which I'm going to refer to
+ * throughout as a "heapoid") in the array, by starting it off at
+ * the bottom of the array and assimilating the array elements
+ * into it one by one until it fills the whole array. Then in the
+ * second phase, the heapoid is dismantled one element at a time,
+ * in such a way that the element extracted at every step is the
+ * largest and so the array is left in sorted order once the
+ * heapoid has shrunk to nothingness.
+ *
+ * The difference between smoothsort and heapsort is that
+ * smoothsort's heapoid is completely different from heapsort's
+ * binary heap. In smoothsort, the heapoid is composed of
+ * sub-blocks with sizes based on Fibonacci numbers, each with a
+ * similar internal substructure (giving the tree-like nature
+ * required of a heapoid), and is in general arranged with the
+ * largest numbers at the _top_ rather than the bottom.
+ *
+ * In particular, this means that smoothsort only ever exchanges a
+ * pair of elements when doing so brings them into their correct
+ * order if they weren't already (unlike heapsort, which starts by
+ * deliberately seeking out the array's largest element and
+ * putting it at the _bottom_). This property is what gives
+ * smoothsort its good performance on already sorted or
+ * nearly-sorted lists.
+ *
+ * The structure of the heapoid
+ * ----------------------------
+ * 
+ * I said above that the heapoid was composed of sub-blocks
+ * (Dijkstra calls them "stretches") whose sizes were "based on"
+ * Fibonacci numbers. In fact, they're what Dijkstra calls
+ * "Leonardo numbers", which have the property that each one is
+ * equal to the sum of the previous two _plus one_. So they go 1,
+ * 1, 3, 5, 9, 15, 25, 41, ... (They're closely related to
+ * Fibonacci numbers, though: if you take the Fibonacci sequence,
+ * multiply each number in it by two and subtract one, you end up
+ * with the Leonardo numbers.)
+ *
+ * In general, the heapoid consists of a series of stretches, each
+ * of which has length equal to some Leonardo number. The
+ * stretches are arranged in descending order of size, and all
+ * their sizes are distinct, with the sole exception that there
+ * are allowed to be two stretches of size 1 (because the number 1
+ * occurs twice in the Leonardo sequence shown above). Each
+ * non-trivial stretch (i.e. larger than one element) is
+ * considered to consist of a root element at the right, and below
+ * that two sub-stretches with sizes equal to the previous two
+ * Leonardo numbers. So a 9-stretch, for instance, could be broken
+ * up into a 5-stretch followed by a 3-stretch followed by a root
+ * element.
+ *
+ * To add a new element to the end of the heapoid during the
+ * building phase, we do one of two things. If the last (shortest)
+ * two stretches already in the heapoid have lengths equal to
+ * _adjacent_ Leonardo numbers in the sequence, then we can
+ * combine the two with our new element to form a stretch of the
+ * next size up. If that isn't the case, then we simply append our
+ * new number to the end as a stretch of size 1. (Dijkstra proves,
+ * so you don't have to, that this always works and that the first
+ * case never ends up forming a second stretch of a size you've
+ * already got.)
+ *
+ * To remove an element from the heapoid during the dismantling
+ * phase, we do the inverse of those two cases: if the last
+ * stretch currently in the heapoid is of size 1 then we simply
+ * remove it, and otherwise we break up the smallest stretch into
+ * its two substretches and take off the root element.
+ *
+ * The ordering constraints on the heapoid
+ * ---------------------------------------
+ *
+ * When the heapoid is fully built, it satisfies two ordering
+ * constraints. Firstly, the root element of each stretch is the
+ * largest element in it, and the same applies to its substretches
+ * and their substretches and so on. In other words, each stretch
+ * can be considered as a (slightly unbalanced) binary tree which
+ * has the _heap property_ (that no child is larger than its
+ * parent) throughout. Secondly, the root elements of all the
+ * (top-level) stretches are in increasing order; so in
+ * particular, the last element in the heapoid (i.e. the root of
+ * the final stretch) is always the largest element in the entire
+ * structure.
+ *
+ * These ordering constraints are preserved throughout the
+ * dismantling phase of the sort. However, during the build phase,
+ * slightly weaker conditions are maintained in order to save some
+ * unnecessary work. Specifically, the last stretch in the heapoid
+ * is not required to have its root be the largest thing in it
+ * (though its substretches, if any, must be properly ordered).
+ * Also, not all the stretches are required to have their roots in
+ * increasing order: instead, only those stretches which are as
+ * big as they can get without overflowing the array must be
+ * correctly ordered (since any other stretch will eventually end
+ * up being combined with something else, so ordering its root
+ * would just be a waste of time that would only have to be
+ * redone). This is always some initial subsequence of the
+ * stretches.
+ *
+ * These ordering constraints are maintained by two procedures
+ * which look a lot like the maintenance functions for a binary
+ * heap.
+ *
+ * The first procedure, which Dijkstra calls "sift", starts with a
+ * potentially misordered element at the root of some stretch, and
+ * moves it down into the stretch until it's correctly positioned.
+ * It looks very like the corresponding operation on a binary
+ * heap: you see if the element is larger than both its children,
+ * and if not you swap it with its larger child and (tail-)recurse
+ * into that substretch. (In fact, the sift operation is actually
+ * slightly _simpler_ than the binary heap version. In a binary
+ * heap you have to worry about the special case where a parent
+ * element has only one child; in smoothsort's heapoid every
+ * element has either two children or none.)
+ *
+ * The second procedure, which Dijkstra refers to by the exciting
+ * non-word "trinkle", does a similar thing but also adds the
+ * stretch to the subsequence whose roots are in increasing order.
+ * It actually looks very similar in concept: it just considers
+ * the root element of each stretch to have (up to) _three_
+ * children, which consist of its obvious two children inside its
+ * own stretch and also the root of the previous stretch. All of
+ * these three children want to be smaller than the root element;
+ * so we find the largest of those children, swap it with the root
+ * if necessary, and (tail-)recurse to there. If we recursed to
+ * the root of the previous stretch, then we are still trinkling
+ * and must do another three-way compare in the next iteration; if
+ * we descended into a substretch then we have finished doing
+ * three-way compares and can revert to a simple sift.
+ *
+ * We use these procedures as follows:
+ *
+ * At the start of each iteration of the build phase, our last
+ * stretch has its root element potentially out of order. We're
+ * either going to add a new stretch after it or turn it into a
+ * substretch of something, so we must certainly fix this by
+ * either doing a sift or a trinkle starting from its root. If
+ * we're subsuming it into a larger stretch, we need only sift; if
+ * there's room to _later_ subsume it into a larger stretch, we
+ * again need only sift; but if we're adding a new 1-stretch after
+ * it and it's of the maximum size that will fit, then we must
+ * trinkle. (This is why we allowed ourselves to leave its root
+ * element unordered at the end of the previous build-phase
+ * iteration: by waiting until we know whether we're merging the
+ * stretch or not, we can avoid doing an unnecessary trinkle when
+ * a sift would have been sufficient.)
+ *
+ * At the end of the build phase, our very last stretch has its
+ * root element potentially wrong, so we must trinkle that.
+ *
+ * During the dismantle phase, the case where we remove a
+ * 1-stretch from the end of the heapoid is harmless and cannot
+ * destroy the ordering properties. The only hard case is the one
+ * where we break up a larger stretch into two substretches and a
+ * root element which we discard. In this case, the root elements
+ * of the two exposed substretches will not necessarily be in the
+ * right order compared to the roots of all the other stretches;
+ * so we must trinkle them both, starting with the larger (lower)
+ * one. (Well, actually, we can do a slightly optimised trinkle
+ * which Dijkstra calls a "semitrinkle": because each of those
+ * substretches was correctly ordered within itself, the first
+ * iteration of the trinkle can be simplified by not bothering to
+ * compare the root element with its two internal children, since
+ * we already know it's larger.)
+ *
+ * That's it!
+ * ----------
+ *
+ * That's the conceptual structure of the smoothsort algorithm. If
+ * you want to try reading Dijkstra's original description, then
+ * (at the time of writing) it's available at
+ *
+ *   http://www.cs.utexas.edu/users/EWD/transcriptions/EWD07xx/EWD796a.html
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stddef.h>
