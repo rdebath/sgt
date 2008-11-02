6ffdddfc6bad1c16b973b5022f58ce144ba051c8
Simon Tatham <anakin@pobox.com>
2008-11-02 11:00:28 +0000

    Nearly forgot: leave out individual files in the text display.
    git-svn-id: svn://svn.tartarus.org/sgt@8253 cda61777-01e9-0310-a592-d414129be87e

--- agedu.c
+++ agedu.c
@@ -179,6 +179,9 @@ static void text_query(const void *mappedfile, const char *querydir,
     s1 = index_query(mappedfile, xi1, t);
     s2 = index_query(mappedfile, xi2, t);
 
+    if (xi2 - xi1 == 1)
+	return;			       /* file, or empty dir => no display */
+
     if (s1 == s2)
 	return;			       /* no space taken up => no display */
 
