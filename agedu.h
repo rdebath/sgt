8e713365242926bfee8591c573660dc8369986ce
Simon Tatham <anakin@pobox.com>
2008-11-03 22:16:30 +0000

    Use the configure output to select an lstat, and to select a method of scanning directories. Everything else can wait until somebody complains - not least because when they do I'll know what sort of system I need to test the required changes on...
    git-svn-id: svn://svn.tartarus.org/sgt@8268 cda61777-01e9-0310-a592-d414129be87e

--- agedu.h
+++ agedu.h
@@ -11,3 +11,11 @@
 #define lenof(x) (sizeof((x))/sizeof(*(x)))
 
 extern char pathsep;
+
+#ifdef HAVE_LSTAT64
+#define STRUCT_STAT struct stat64
+#define LSTAT lstat64
+#else
+#define STRUCT_STAT struct stat
+#define LSTAT lstat
+#endif
---
+++ config.h
@@ -0,0 +1,15 @@
+/*
+ * config.h: stub version of the autoconf-generated config.h, to
+ * stop the #include in agedu.h from failing when run from the
+ * master source directory instead of from the unpacked contents
+ * of the autotoolsified tarball.
+ *
+ * This version of config.h hardwires some parameters that match
+ * the sorts of systems I tend to be building on. A nasty hack and
+ * a self-centred one, but autotools is so icky that I can't quite
+ * bring myself to inflict it on my main development directory,
+ * and am instead keeping it at arm's length in the distribution
+ * tarballs. Sorry about that.
+ */
+
+#define HAVE_LSTAT64
