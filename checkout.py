47864857f1dcdcff14196fbcff8fea38220b2d2b
Simon Tatham <anakin@pobox.com>
2009-12-07 18:45:23 +0000

    I'm now occasionally using git-svn as an alternative svn client, so let's have support for that in bob.
    git-svn-id: svn://svn.tartarus.org/sgt@8774 cda61777-01e9-0310-a592-d414129be87e

--- checkout.py
+++ checkout.py
@@ -7,6 +7,7 @@ import lexer
 import script
 import misc
 import os
+import shutil
 
 headrevs = {}
 
@@ -35,19 +36,67 @@ def checkout(cfg, module, path, is_main):
 
     set_headrev = 0
 
+    git = 0
+
     if details[2] != None:
         # If we've been given an actual working directory, we just
         # do an export of that.
         svnparams = [details[2]]
 
+        log.logmsg("  Using existing working directory %s" % details[2])
+
         # Also, determine the revision number of the working
         # directory using svnversion.
-        svnvcmd = misc.shellquote(["svnversion", details[2]])
-        f = os.popen(svnvcmd + " 2>&1", "r")
-        newrev = f.read()
-        f.close()
-        while newrev[-1:] == "\r" or newrev[-1:] == "\n":
-            newrev = newrev[:-1]
+        if os.access(details[2]+"/.svn", os.F_OK):
+            svnvcmd = misc.shellquote(["svnversion", details[2]])
+            f = os.popen(svnvcmd + " 2>&1", "r")
+            newrev = f.read()
+            f.close()
+            while newrev[-1:] == "\r" or newrev[-1:] == "\n":
+                newrev = newrev[:-1]
+            log.logmsg("  Revision returned from svnversion: %s" % newrev)
+        elif os.access(details[2]+"/.git", os.F_OK):
+            git = 1
+            cdcmd = misc.shellquote(["cd", details[2]])
+
+            gitstatcmd = misc.shellquote(["git", "status"])
+            f = os.popen(cdcmd + "&&" + gitstatcmd + " 2>&1", "r")
+            mod = "M" # assume modified unless git status reports clean
+            while 1:
+                s = f.readline()
+                if s == "":
+                    break
+                if s[-1:] == "\n": s = s[:-1]
+                if s[:8] == "nothing ":
+                    mod = ""
+            f.close()
+
+            gitlogcmd = misc.shellquote(["git", "log"])
+            f = os.popen(cdcmd + "&&" + gitlogcmd + " 2>&1", "r")
+            first = 1
+            while 1:
+                s = f.readline()
+                if s == "":
+                    raise misc.builderr("working directory `%s': unable to find git-svn latest revision in `git log' output")
+                if s[-1:] == "\n": s = s[:-1]
+                if s[:16] == "    git-svn-id: ":
+                    ss = string.split(s)
+                    if len(ss) > 1:
+                        try:
+                            i = string.rindex(ss[1], "@")
+                            newrev = ss[1][i+1:] + mod
+                            break
+                        except ValueError, e:
+                            pass
+                if s[:6] == "commit":
+                    if first:
+                        first = 0
+                    else:
+                        mod = "M"
+            f.close()
+            log.logmsg("  Revision faked via git-svn: %s" % newrev)
+        else:
+            raise misc.builderr("working directory `%s' is not a Subversion working copy")
 
         # If there's more than one revision represented here, raise
         # an error unless we've been told to accept that.
@@ -111,50 +160,73 @@ def checkout(cfg, module, path, is_main):
 
         lcrevindex = (-1, svnrepos)
 
-    # Now we can construct the exact svn export command we want to run.
-    svncmdlist = ["svn", "export"] + svnparams + [path]
-    svncmdstr = misc.shellquote(svncmdlist)
-    log.logmsg("  Running export command: " + svncmdstr)
+    if not git:
+        # Now we can construct the exact svn export command we want to run.
+        svncmdlist = ["svn", "export"] + svnparams + [path]
+        svncmdstr = misc.shellquote(svncmdlist)
+        log.logmsg("  Running export command: " + svncmdstr)
 
-    # Now actually run the command. We retrieve the output, write
-    # it into the log file, and scan it to see if it mentions a
-    # revision number.
-    f = os.popen(svncmdstr + " 2>&1", "r")
-    while 1:
-        line = f.readline()
-        if line == "": break
-        while line[-1:] == "\r" or line[-1:] == "\n":
-            line = line[:-1]
-        log.logoutput(line)
-        if line[:18] == "Exported revision " and line[-1:] == ".":
-            newrev = line[18:-1]
-    ret = f.close()
-    if ret > 0:
-        raise misc.builderr("svn export command terminated with status %d" % ret)
+        # Now actually run the command. We retrieve the output, write
+        # it into the log file, and scan it to see if it mentions a
+        # revision number.
+        f = os.popen(svncmdstr + " 2>&1", "r")
+        while 1:
+            line = f.readline()
+            if line == "": break
+            while line[-1:] == "\r" or line[-1:] == "\n":
+                line = line[:-1]
+            log.logoutput(line)
+            if line[:18] == "Exported revision " and line[-1:] == ".":
+                newrev = line[18:-1]
+        ret = f.close()
+        if ret > 0:
+            raise misc.builderr("svn export command terminated with status %d" % ret)
 
-    if newrev == "":
-        raise misc.builderr("Unable to determine revision number for checked-out module `%s'" % module)
-    log.logmsg("  Revision number for this module is %s" % newrev)
-    if lcrevindex[0] == -1:
-        lcrevindex = (newrev,) + lcrevindex[1:]
+        if newrev == "":
+            raise misc.builderr("Unable to determine revision number for checked-out module `%s'" % module)
+        log.logmsg("  Revision number for this module is %s" % newrev)
+        if lcrevindex[0] == -1:
+            lcrevindex = (newrev,) + lcrevindex[1:]
 
-    # We'll also run `svn info' to get the last-changed revision.
-    svnicmd = misc.shellquote(["svn", "info"] + svnparams)
-    log.logmsg("  Running info command: " + svnicmd)
-    f = os.popen(svnicmd + " 2>&1", "r")
-    lcrev = None
-    lcout = ""
-    while 1:
-        line = f.readline()
-        if line == "": break
-        while line[-1:] == "\r" or line[-1:] == "\n":
-            line = line[:-1]
-        log.logoutput(line)
-        if line[:18] == "Last Changed Rev: ":
-            lcrev = string.atoi(line[18:])
-    ret = f.close()
-    if ret > 0:
-        raise misc.builderr("svn info command terminated with status %d" % ret)
+        # We'll also run `svn info' to get the last-changed revision.
+        svnicmd = misc.shellquote(["svn", "info"] + svnparams)
+        log.logmsg("  Running info command: " + svnicmd)
+        f = os.popen(svnicmd + " 2>&1", "r")
+        lcrev = None
+        lcout = ""
+        while 1:
+            line = f.readline()
+            if line == "": break
+            while line[-1:] == "\r" or line[-1:] == "\n":
+                line = line[:-1]
+            log.logoutput(line)
+            if line[:18] == "Last Changed Rev: ":
+                lcrev = string.atoi(line[18:])
+        ret = f.close()
+        if ret > 0:
+            raise misc.builderr("svn info command terminated with status %d" % ret)
+    else:
+        # Manually fake up an 'svn export' via git.
+        cdcmd = misc.shellquote(["cd", details[2]])
+        gitlogcmd = misc.shellquote(["git", "ls-files"])
+        f = os.popen(cdcmd + "&&" + gitlogcmd + " 2>&1", "r")
+        while 1:
+            s = f.readline()
+            if s == "":
+                break
+            if s[-1:] == "\n": s = s[:-1]
+            p = path + "/" + s
+            i = string.rindex(p, "/")
+            try:
+                os.makedirs(p[:i])
+            except OSError, e:
+                pass # already exists
+            shutil.copyfile(details[2] + "/" + s, p)
+        f.close()
+        lcrev = newrev
+        if lcrev[-1:] == "M":
+            lcrev = lcrev[:-1]
+        lcrev = string.atoi(lcrev)
 
     if lcrev == None:
         raise misc.builderr("Unable to determine last-changed revision number for checked-out module `%s'" % module)
