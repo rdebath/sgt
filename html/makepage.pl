4f21983568998ee3a1db115f0c6cde1e4147c598
Simon Tatham <anakin@pobox.com>
2010-05-10 18:10:25 +0000

    Revamp the HTML page generation to render all the polyhedra as interactive models using canvaspoly.js, falling back to the previous static images if canvases/JS are not supported.
    git-svn-id: svn://svn.tartarus.org/sgt@8945 cda61777-01e9-0310-a592-d414129be87e

---
+++ html/makepage.pl
@@ -0,0 +1,35 @@
+#!/usr/bin/perl 
+
+open IN, "index.tmpl";
+open OUT, ">index.html";
+
+while (<IN>) {
+    s/<poly name="([^\"]+)">/&dopoly($1)/eg;
+    s/<image name="([^\"]+)">/&doimage($1)/eg;
+    print OUT $_;
+}
+
+close IN;
+close OUT;
+
+sub dopoly($) {
+    my $name = shift @_;
+    chomp($desc = `cat ${name}.desc`);
+    $img = &doimage($name); # HACK HACK HACK: sets globals $x and $y
+    return "<canvas class=\"polyhedron\" width=\"$x\" height=\"$y\" ".
+      "data=\"$desc\">\n" .
+      "$img\n" .
+      "</canvas>";
+}
+
+sub doimage($) {
+    my $name = shift @_;
+    $image = "${name}.png";
+    $k = `identify $image`;
+    $k =~ /PNG (\d+)x(\d+)/ and do { $x = $1; $y = $2; };
+    @st = stat $image;
+    $alt = sprintf "[%dx%d greyscale PNG, %dKB]",
+      $x, $y, int(($st[7] + 1023) / 1024);
+    return "<a href=\"$image\"><img src=\"$image\" alt=\"$alt\" ".
+      "width=\"$x\" height=\"$y\" border=\"0\"></a>";
+}
