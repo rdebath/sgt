77d8e0a593f2030715c7ce18e7efe308e602cdcb
Simon Tatham <anakin@pobox.com>
2012-11-10 11:47:56 +0000

    New system for time-syncing the Chumby, since I've just noticed that the previous approach of using time.chumby.com has stopped working since that server no longer answers.
    git-svn-id: svn://svn.tartarus.org/sgt@9700 cda61777-01e9-0310-a592-d414129be87e

--- tring/install.sh
+++ tring/install.sh
@@ -6,7 +6,7 @@ if test $# -gt 0; then
   # Given a hostname on the command line, remotely install to its
   # /mnt/usb. Saves unplugging the USB key!
   lns build/tring tringtmp
-  scp tringtmp debugchumby sshkeys excurl "$1":/mnt/usb
+  scp tringtmp debugchumby sshkeys excurl timeip "$1":/mnt/usb
   ssh "$1" mv /mnt/usb/tring{tmp,}
   rm tringtmp
   ssh "$1" sync
@@ -16,6 +16,7 @@ else
     cp debugchumby /mnt
     cp sshkeys /mnt
     cp excurl /mnt
+    cp timeip /mnt
     umount /mnt
     sync
     sleep 2
