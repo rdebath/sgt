981943c18393af44829807a81eabd16c6fe68c68
Simon Tatham <anakin@pobox.com>
2013-07-10 20:11:22 +0000

    Translate box-drawing characters into +-|.
    git-svn-id: svn://svn.tartarus.org/sgt@9900 cda61777-01e9-0310-a592-d414129be87e

--- filter/asciify.c
+++ filter/asciify.c
@@ -1967,6 +1967,145 @@ if (@acs) {
                   case 0x00D7: outptr = "x"; break; /* MULTIPLICATION SIGN (should this be * for maths contexts, or x for 1024x768 contexts?) */
                   case 0x00F7: outptr = "/"; break; /* DIVISION SIGN (here I have no better option, though it's a shame it doesn't match my choice for D7) */
 
+                    /*
+                     * Box drawing characters.
+                     */
+                  case 0x2508: /* BOX DRAWINGS LIGHT QUADRUPLE DASH HORIZONTAL */
+                  case 0x2509: /* BOX DRAWINGS HEAVY QUADRUPLE DASH HORIZONTAL */
+                  case 0x250C: /* BOX DRAWINGS LIGHT DOWN AND RIGHT */
+                  case 0x250D: /* BOX DRAWINGS DOWN LIGHT AND RIGHT HEAVY */
+                  case 0x250E: /* BOX DRAWINGS DOWN HEAVY AND RIGHT LIGHT */
+                  case 0x250F: /* BOX DRAWINGS HEAVY DOWN AND RIGHT */
+                  case 0x2510: /* BOX DRAWINGS LIGHT DOWN AND LEFT */
+                  case 0x2511: /* BOX DRAWINGS DOWN LIGHT AND LEFT HEAVY */
+                  case 0x2512: /* BOX DRAWINGS DOWN HEAVY AND LEFT LIGHT */
+                  case 0x2513: /* BOX DRAWINGS HEAVY DOWN AND LEFT */
+                  case 0x2514: /* BOX DRAWINGS LIGHT UP AND RIGHT */
+                  case 0x2515: /* BOX DRAWINGS UP LIGHT AND RIGHT HEAVY */
+                  case 0x2516: /* BOX DRAWINGS UP HEAVY AND RIGHT LIGHT */
+                  case 0x2517: /* BOX DRAWINGS HEAVY UP AND RIGHT */
+                  case 0x2518: /* BOX DRAWINGS LIGHT UP AND LEFT */
+                  case 0x2519: /* BOX DRAWINGS UP LIGHT AND LEFT HEAVY */
+                  case 0x251A: /* BOX DRAWINGS UP HEAVY AND LEFT LIGHT */
+                  case 0x251B: /* BOX DRAWINGS HEAVY UP AND LEFT */
+                  case 0x251C: /* BOX DRAWINGS LIGHT VERTICAL AND RIGHT */
+                  case 0x251D: /* BOX DRAWINGS VERTICAL LIGHT AND RIGHT HEAVY */
+                  case 0x251E: /* BOX DRAWINGS UP HEAVY AND RIGHT DOWN LIGHT */
+                  case 0x251F: /* BOX DRAWINGS DOWN HEAVY AND RIGHT UP LIGHT */
+                  case 0x2520: /* BOX DRAWINGS VERTICAL HEAVY AND RIGHT LIGHT */
+                  case 0x2521: /* BOX DRAWINGS DOWN LIGHT AND RIGHT UP HEAVY */
+                  case 0x2522: /* BOX DRAWINGS UP LIGHT AND RIGHT DOWN HEAVY */
+                  case 0x2523: /* BOX DRAWINGS HEAVY VERTICAL AND RIGHT */
+                  case 0x2524: /* BOX DRAWINGS LIGHT VERTICAL AND LEFT */
+                  case 0x2525: /* BOX DRAWINGS VERTICAL LIGHT AND LEFT HEAVY */
+                  case 0x2526: /* BOX DRAWINGS UP HEAVY AND LEFT DOWN LIGHT */
+                  case 0x2527: /* BOX DRAWINGS DOWN HEAVY AND LEFT UP LIGHT */
+                  case 0x2528: /* BOX DRAWINGS VERTICAL HEAVY AND LEFT LIGHT */
+                  case 0x2529: /* BOX DRAWINGS DOWN LIGHT AND LEFT UP HEAVY */
+                  case 0x252A: /* BOX DRAWINGS UP LIGHT AND LEFT DOWN HEAVY */
+                  case 0x252B: /* BOX DRAWINGS HEAVY VERTICAL AND LEFT */
+                  case 0x252C: /* BOX DRAWINGS LIGHT DOWN AND HORIZONTAL */
+                  case 0x252D: /* BOX DRAWINGS LEFT HEAVY AND RIGHT DOWN LIGHT */
+                  case 0x252E: /* BOX DRAWINGS RIGHT HEAVY AND LEFT DOWN LIGHT */
+                  case 0x252F: /* BOX DRAWINGS DOWN LIGHT AND HORIZONTAL HEAVY */
+                  case 0x2530: /* BOX DRAWINGS DOWN HEAVY AND HORIZONTAL LIGHT */
+                  case 0x2531: /* BOX DRAWINGS RIGHT LIGHT AND LEFT DOWN HEAVY */
+                  case 0x2532: /* BOX DRAWINGS LEFT LIGHT AND RIGHT DOWN HEAVY */
+                  case 0x2533: /* BOX DRAWINGS HEAVY DOWN AND HORIZONTAL */
+                  case 0x2534: /* BOX DRAWINGS LIGHT UP AND HORIZONTAL */
+                  case 0x2535: /* BOX DRAWINGS LEFT HEAVY AND RIGHT UP LIGHT */
+                  case 0x2536: /* BOX DRAWINGS RIGHT HEAVY AND LEFT UP LIGHT */
+                  case 0x2537: /* BOX DRAWINGS UP LIGHT AND HORIZONTAL HEAVY */
+                  case 0x2538: /* BOX DRAWINGS UP HEAVY AND HORIZONTAL LIGHT */
+                  case 0x2539: /* BOX DRAWINGS RIGHT LIGHT AND LEFT UP HEAVY */
+                  case 0x253A: /* BOX DRAWINGS LEFT LIGHT AND RIGHT UP HEAVY */
+                  case 0x253B: /* BOX DRAWINGS HEAVY UP AND HORIZONTAL */
+                  case 0x253C: /* BOX DRAWINGS LIGHT VERTICAL AND HORIZONTAL */
+                  case 0x253D: /* BOX DRAWINGS LEFT HEAVY AND RIGHT VERTICAL LIGHT */
+                  case 0x253E: /* BOX DRAWINGS RIGHT HEAVY AND LEFT VERTICAL LIGHT */
+                  case 0x253F: /* BOX DRAWINGS VERTICAL LIGHT AND HORIZONTAL HEAVY */
+                  case 0x2540: /* BOX DRAWINGS UP HEAVY AND DOWN HORIZONTAL LIGHT */
+                  case 0x2541: /* BOX DRAWINGS DOWN HEAVY AND UP HORIZONTAL LIGHT */
+                  case 0x2542: /* BOX DRAWINGS VERTICAL HEAVY AND HORIZONTAL LIGHT */
+                  case 0x2543: /* BOX DRAWINGS LEFT UP HEAVY AND RIGHT DOWN LIGHT */
+                  case 0x2544: /* BOX DRAWINGS RIGHT UP HEAVY AND LEFT DOWN LIGHT */
+                  case 0x2545: /* BOX DRAWINGS LEFT DOWN HEAVY AND RIGHT UP LIGHT */
+                  case 0x2546: /* BOX DRAWINGS RIGHT DOWN HEAVY AND LEFT UP LIGHT */
+                  case 0x2547: /* BOX DRAWINGS DOWN LIGHT AND UP HORIZONTAL HEAVY */
+                  case 0x2548: /* BOX DRAWINGS UP LIGHT AND DOWN HORIZONTAL HEAVY */
+                  case 0x2549: /* BOX DRAWINGS RIGHT LIGHT AND LEFT VERTICAL HEAVY */
+                  case 0x254A: /* BOX DRAWINGS LEFT LIGHT AND RIGHT VERTICAL HEAVY */
+                  case 0x254B: /* BOX DRAWINGS HEAVY VERTICAL AND HORIZONTAL */
+                  case 0x2552: /* BOX DRAWINGS DOWN SINGLE AND RIGHT DOUBLE */
+                  case 0x2553: /* BOX DRAWINGS DOWN DOUBLE AND RIGHT SINGLE */
+                  case 0x2554: /* BOX DRAWINGS DOUBLE DOWN AND RIGHT */
+                  case 0x2555: /* BOX DRAWINGS DOWN SINGLE AND LEFT DOUBLE */
+                  case 0x2556: /* BOX DRAWINGS DOWN DOUBLE AND LEFT SINGLE */
+                  case 0x2557: /* BOX DRAWINGS DOUBLE DOWN AND LEFT */
+                  case 0x2558: /* BOX DRAWINGS UP SINGLE AND RIGHT DOUBLE */
+                  case 0x2559: /* BOX DRAWINGS UP DOUBLE AND RIGHT SINGLE */
+                  case 0x255A: /* BOX DRAWINGS DOUBLE UP AND RIGHT */
+                  case 0x255B: /* BOX DRAWINGS UP SINGLE AND LEFT DOUBLE */
+                  case 0x255C: /* BOX DRAWINGS UP DOUBLE AND LEFT SINGLE */
+                  case 0x255D: /* BOX DRAWINGS DOUBLE UP AND LEFT */
+                  case 0x255E: /* BOX DRAWINGS VERTICAL SINGLE AND RIGHT DOUBLE */
+                  case 0x255F: /* BOX DRAWINGS VERTICAL DOUBLE AND RIGHT SINGLE */
+                  case 0x2560: /* BOX DRAWINGS DOUBLE VERTICAL AND RIGHT */
+                  case 0x2561: /* BOX DRAWINGS VERTICAL SINGLE AND LEFT DOUBLE */
+                  case 0x2562: /* BOX DRAWINGS VERTICAL DOUBLE AND LEFT SINGLE */
+                  case 0x2563: /* BOX DRAWINGS DOUBLE VERTICAL AND LEFT */
+                  case 0x2564: /* BOX DRAWINGS DOWN SINGLE AND HORIZONTAL DOUBLE */
+                  case 0x2565: /* BOX DRAWINGS DOWN DOUBLE AND HORIZONTAL SINGLE */
+                  case 0x2566: /* BOX DRAWINGS DOUBLE DOWN AND HORIZONTAL */
+                  case 0x2567: /* BOX DRAWINGS UP SINGLE AND HORIZONTAL DOUBLE */
+                  case 0x2568: /* BOX DRAWINGS UP DOUBLE AND HORIZONTAL SINGLE */
+                  case 0x2569: /* BOX DRAWINGS DOUBLE UP AND HORIZONTAL */
+                  case 0x256A: /* BOX DRAWINGS VERTICAL SINGLE AND HORIZONTAL DOUBLE */
+                  case 0x256B: /* BOX DRAWINGS VERTICAL DOUBLE AND HORIZONTAL SINGLE */
+                  case 0x256C: /* BOX DRAWINGS DOUBLE VERTICAL AND HORIZONTAL */
+                  case 0x256D: /* BOX DRAWINGS LIGHT ARC DOWN AND RIGHT */
+                  case 0x256E: /* BOX DRAWINGS LIGHT ARC DOWN AND LEFT */
+                  case 0x256F: /* BOX DRAWINGS LIGHT ARC UP AND LEFT */
+                  case 0x2570: /* BOX DRAWINGS LIGHT ARC UP AND RIGHT */
+                  case 0x2571: /* BOX DRAWINGS LIGHT DIAGONAL UPPER RIGHT TO LOWER LEFT */
+                  case 0x2572: /* BOX DRAWINGS LIGHT DIAGONAL UPPER LEFT TO LOWER RIGHT */
+                    outptr = "+";
+                    break;
+
+                  case 0x2502: /* BOX DRAWINGS LIGHT VERTICAL */
+                  case 0x2503: /* BOX DRAWINGS HEAVY VERTICAL */
+                  case 0x2506: /* BOX DRAWINGS LIGHT TRIPLE DASH VERTICAL */
+                  case 0x2507: /* BOX DRAWINGS HEAVY TRIPLE DASH VERTICAL */
+                  case 0x250A: /* BOX DRAWINGS LIGHT QUADRUPLE DASH VERTICAL */
+                  case 0x250B: /* BOX DRAWINGS HEAVY QUADRUPLE DASH VERTICAL */
+                  case 0x254E: /* BOX DRAWINGS LIGHT DOUBLE DASH VERTICAL */
+                  case 0x254F: /* BOX DRAWINGS HEAVY DOUBLE DASH VERTICAL */
+                  case 0x2551: /* BOX DRAWINGS DOUBLE VERTICAL */
+                  case 0x2575: /* BOX DRAWINGS LIGHT UP */
+                  case 0x2577: /* BOX DRAWINGS LIGHT DOWN */
+                  case 0x2579: /* BOX DRAWINGS HEAVY UP */
+                  case 0x257B: /* BOX DRAWINGS HEAVY DOWN */
+                  case 0x257D: /* BOX DRAWINGS LIGHT UP AND HEAVY DOWN */
+                  case 0x257F: /* BOX DRAWINGS HEAVY UP AND LIGHT DOWN */
+                    outptr = "|";
+                    break;
+
+                  case 0x2500: /* BOX DRAWINGS LIGHT HORIZONTAL */
+                  case 0x2501: /* BOX DRAWINGS HEAVY HORIZONTAL */
+                  case 0x2504: /* BOX DRAWINGS LIGHT TRIPLE DASH HORIZONTAL */
+                  case 0x2505: /* BOX DRAWINGS HEAVY TRIPLE DASH HORIZONTAL */
+                  case 0x254C: /* BOX DRAWINGS LIGHT DOUBLE DASH HORIZONTAL */
+                  case 0x254D: /* BOX DRAWINGS HEAVY DOUBLE DASH HORIZONTAL */
+                  case 0x2550: /* BOX DRAWINGS DOUBLE HORIZONTAL */
+                  case 0x2574: /* BOX DRAWINGS LIGHT LEFT */
+                  case 0x2576: /* BOX DRAWINGS LIGHT RIGHT */
+                  case 0x2578: /* BOX DRAWINGS HEAVY LEFT */
+                  case 0x257A: /* BOX DRAWINGS HEAVY RIGHT */
+                  case 0x257C: /* BOX DRAWINGS LIGHT LEFT AND HEAVY RIGHT */
+                  case 0x257E: /* BOX DRAWINGS HEAVY LEFT AND LIGHT RIGHT */
+                    outptr = "-";
+                    break;
+
                   default:
                     if (wc >= 0 && wc < 0x80) {
                         localbuf[0] = (char)wc;
